d9a1bf88cd88fe58e4b4fd7413dfd3297e108929
divertitoS <astanislav2361@gmail.com>
2021-06-18 16:50:48 +0300

    Implemented AuthenticationController.

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -9,7 +9,9 @@ import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
 import mate.academy.spring.service.dto.mapping.impl.response.OrderResponseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,17 +22,16 @@ import java.util.stream.Collectors;
 public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
-    private final OrderResponseMapper orderResponseMapper;
+    private final DtoResponseMapper<OrderResponseDto, Order> dtoResponseMapper;
     private final ShoppingCartService shoppingCartService;
 
     public OrderController(OrderService orderService,
                            UserService userService,
-                           OrderResponseMapper orderResponseMapper,
-                           ShoppingCartService shoppingCartService) {
+                           ShoppingCartService shoppingCartService, DtoResponseMapper<OrderResponseDto, Order> dtoResponseMapper) {
         this.orderService = orderService;
         this.userService = userService;
-        this.orderResponseMapper = orderResponseMapper;
         this.shoppingCartService = shoppingCartService;
+        this.dtoResponseMapper = dtoResponseMapper;
     }
 
     @GetMapping
@@ -38,7 +39,7 @@ public class OrderController {
         User user = userService.getById(userId);
         List<Order> listOfOrders = orderService.getOrdersHistory(user);
         return listOfOrders.stream()
-                .map(orderResponseMapper::toDto)
+                .map(dtoResponseMapper::toDto)
                 .collect(Collectors.toList());
     }
 
@@ -47,6 +48,6 @@ public class OrderController {
         User currentUser = userService.getById(userId);
         ShoppingCart currentUserShoppingCart = shoppingCartService.getByUser(currentUser);
         Order currentUserOrder = orderService.completeOrder(currentUserShoppingCart);
-        return orderResponseMapper.toDto(currentUserOrder);
+        return dtoResponseMapper.toDto(currentUserOrder);
     }
 }
