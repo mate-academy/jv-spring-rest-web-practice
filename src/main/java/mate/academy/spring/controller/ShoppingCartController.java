a591b59a8ceba503dbab6cfbf64dcf3c2e638d2e
Maksim <max.freedman.main@gmail.com>
2021-06-24 13:21:49 +0300

    Added shopping cart controller

---
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import mate.academy.spring.service.MovieSessionService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.impl.response.ShoppingCartResponseMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shopping-carts")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final MovieSessionService movieSessionService;
+    private final ShoppingCartResponseMapper shoppingCartResponseMapper;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService,
+                                  UserService userService,
+                                  MovieSessionService movieSessionService,
+                                  ShoppingCartResponseMapper shoppingCartResponseMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.movieSessionService = movieSessionService;
+        this.shoppingCartResponseMapper = shoppingCartResponseMapper;
+    }
+
+    @PostMapping("/movie-sessions")
+    public ShoppingCartResponseDto addMovieSession(@RequestParam Long userId,
+                                                   @RequestParam Long movieSessionId) {
+        return null;
+    }
+}
