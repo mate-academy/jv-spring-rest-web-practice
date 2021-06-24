f283330421e5ba5d049d93854308d9908fd6614d
Maksim <max.freedman.main@gmail.com>
2021-06-24 11:54:57 +0300

    Added authenctication controller and added @Component annotation to user response mapper

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import mate.academy.spring.service.dto.mapping.impl.response.UserResponseMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserResponseMapper userResponseMapper;
+
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    UserResponseMapper userResponseMapper) {
+        this.authenticationService = authenticationService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(UserRequestDto userRequestDto) {
+        User registeredUser = authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
+        return userResponseMapper.toDto(registeredUser);
+    }
+}
--- src/main/java/mate/academy/spring/service/dto/mapping/impl/response/UserResponseMapper.java
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/UserResponseMapper.java
@@ -3,7 +3,10 @@ package mate.academy.spring.service.dto.mapping.impl.response;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
 
     @Override
