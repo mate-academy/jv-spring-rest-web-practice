1fe1fb6ac42d6bf4758702c50656fb5ccd316685
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-06-24 13:46:55 +0300

    Added OrderResponseDto+OrderResponseMapper Added OrderController

---
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/OrderResponseMapper.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.service.dto.mapping.impl.response;
+
+import java.util.stream.Collectors;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto responseDto = new OrderResponseDto();
+        responseDto.setId(order.getId());
+        responseDto.setOrderDate(order.getOrderDate().toString());
+        responseDto.setUserId(order.getUser().getId());
+        responseDto.setTicketsId(order.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        return responseDto;
+    }
+}
