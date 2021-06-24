c751ad141be78fc6960a96879aa4018ac4897e32
Maksim <max.freedman.main@gmail.com>
2021-06-24 15:45:16 +0300

    Fixed LazyInit exception and NullPointerException

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,10 +1,13 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.dto.mapping.impl.response.ShoppingCartResponseMapper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +34,15 @@ public class ShoppingCartController {
     @PostMapping("/movie-sessions")
     public ShoppingCartResponseDto addMovieSession(@RequestParam Long userId,
                                                    @RequestParam Long movieSessionId) {
-        return null;
+        User user = userService.get(userId);
+        MovieSession movieSession = movieSessionService.get(movieSessionId);
+        shoppingCartService.addSession(movieSession, user);
+        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(user));
+    }
+
+    @GetMapping("/by-user")
+    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
+        User user = userService.get(userId);
+        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(user));
     }
 }
