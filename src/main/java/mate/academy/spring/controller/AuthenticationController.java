eaa275d13b7b7453ead867c04621876f62ed1fa5
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-06-24 14:21:02 +0300

    Fixed issues.

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    final AuthenticationService authenticationService;
-    final UserResponseMapper userResponseMapper;
+    private final AuthenticationService authenticationService;
+    private final UserResponseMapper userResponseMapper;
 
     public AuthenticationController(AuthenticationService authenticationService,
                                     UserResponseMapper userResponseMapper) {
