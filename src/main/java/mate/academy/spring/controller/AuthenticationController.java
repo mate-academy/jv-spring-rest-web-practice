00cb35f6421ac96b9d7e3c6c4f9ad781d6c79c4c
Дмитро <dima4994@gmail.com>
2021-06-24 09:40:23 +0300

    fixed all bugs notice by Den4eg007! (+checkstyle)

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -9,8 +9,6 @@ import mate.academy.spring.service.dto.mapping.DtoRequestMapper;
 import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -1,10 +1,7 @@
 package mate.academy.spring.model.dto.response;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
-import mate.academy.spring.model.Ticket;
-import mate.academy.spring.model.User;
 
 @Data
 public class OrderResponseDto {
