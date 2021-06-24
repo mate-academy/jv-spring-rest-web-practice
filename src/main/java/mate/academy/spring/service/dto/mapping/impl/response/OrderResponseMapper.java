f63b8b04000e0375d46e40282c7fc66fb293476c
Koziarevskyi <koziarevskyi@gmail.com>
2021-06-24 15:51:35 +0300

    C001 -All Done! For review!

---
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/OrderResponseMapper.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.service.dto.mapping.impl.response;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto,
+        Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto responseDto = new OrderResponseDto();
+        responseDto.setId(order.getId());
+        responseDto.setOrderDate(order.getOrderDate()
+                                      .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+        List<Long> ticketIds = order.getTickets()
+                                    .stream()
+                                    .map(Ticket::getId)
+                                    .collect(Collectors.toList());
+        responseDto.setTicketsId(ticketIds);
+        responseDto.setUserId(order.getUser().getId());
+        return responseDto;
+    }
+}
