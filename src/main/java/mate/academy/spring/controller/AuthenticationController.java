0c14af10060cdcb822d03afa8c1e33e370ad6202
Tetiana Chupika <mtv1301@gmail.com>
2021-10-20 11:02:58 +0300

    added solution to hw spring rest web practice

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.controller;
+
+import jakarta.validation.Valid;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.impl.request.UserRequestMapper;
+import mate.academy.spring.service.dto.mapping.impl.response.UserResponseMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final UserService userService;
+    private final UserRequestMapper userRequestMapper;
+    private final UserResponseMapper userResponseMapper;
+    private final ShoppingCartService shoppingCartService;
+
+    public AuthenticationController(UserService userService, UserRequestMapper userRequestMapper,
+                                    UserResponseMapper userResponseMapper,
+                                    ShoppingCartService shoppingCartService) {
+        this.userService = userService;
+        this.userRequestMapper = userRequestMapper;
+        this.userResponseMapper = userResponseMapper;
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody @Valid UserRequestDto user) {
+        shoppingCartService.registerNewShoppingCart(userRequestMapper.fromDto(user));
+        return userResponseMapper.toDto(userService.add(userRequestMapper.fromDto(user)));
+    }
+}
