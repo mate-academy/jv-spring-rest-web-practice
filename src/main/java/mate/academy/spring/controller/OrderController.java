c2f3064c9113be440667d187e344803e4e91dfc1
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-11-10 17:38:25 +0200

    Fixed

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -4,16 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.User;
-import mate.academy.spring.model.dto.request.OrderRequestDto;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.service.dto.mapping.impl.request.OrderRequestMapper;
 import mate.academy.spring.service.dto.mapping.impl.response.OrderResponseMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,16 +21,14 @@ public class OrderController {
     private final UserService userService;
     private final OrderService orderService;
     private final OrderResponseMapper orderResponseMapper;
-    private final OrderRequestMapper orderRequestMapper;
     private final ShoppingCartService shoppingCartService;
 
     public OrderController(UserService userService,
-                   OrderService orderService, OrderResponseMapper orderResponseMapper,
-                   OrderRequestMapper orderRequestMapper, ShoppingCartService shoppingCartService) {
+              OrderService orderService, OrderResponseMapper orderResponseMapper,
+                           ShoppingCartService shoppingCartService) {
         this.userService = userService;
         this.orderService = orderService;
         this.orderResponseMapper = orderResponseMapper;
-        this.orderRequestMapper = orderRequestMapper;
         this.shoppingCartService = shoppingCartService;
     }
 
@@ -45,10 +40,7 @@ public class OrderController {
     }
 
     @PostMapping("/complete")
-    public OrderResponseDto completeOrder(@RequestParam Long userId,
-                      @RequestBody OrderRequestDto orderRequestDto) {
-        Order order = orderRequestMapper.fromDto(orderRequestDto);
-        order.setUser(userService.get(userId));
+    public OrderResponseDto completeOrder(@RequestParam Long userId) {
         Order completeOrder = orderService.completeOrder(
                 shoppingCartService.getByUser(userService.get(userId)));
         return orderResponseMapper.toDto(completeOrder);
