94318b6d1402983576ebb41f40aa84435dbdacec
Vladyslav Gural <vladgural@gmail.com>
2021-11-11 18:06:31 +0200

    changed type field from LocalDateTine to String in OrderResponseDto

--- src/main/java/mate/academy/spring/service/dto/mapping/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/OrderResponseMapper.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.service.dto.mapping.impl.response;
 
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.Ticket;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderResponseMapper implements
         DtoResponseMapper<OrderResponseDto, Order> {
+    private static final String DATE_PATTERN = "dd.MM.yyyy HH:mm";
 
     @Override
     public OrderResponseDto toDto(Order order) {
         OrderResponseDto orderResponseDto = new OrderResponseDto();
         orderResponseDto.setId(order.getId());
-        orderResponseDto.setOrderDate(order.getOrderDate());
+        orderResponseDto.setOrderDate(order.getOrderDate()
+                .format(DateTimeFormatter.ofPattern(DATE_PATTERN)));
         orderResponseDto.setUserId(order.getUser().getId());
         orderResponseDto.setTicketIds(order.getTickets().stream()
                 .map(Ticket::getId)
