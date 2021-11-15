c27e08de56450f4ff4bc7607ff86b08ba360614e
Pasha Rakitin <dogovor@lan.com.ua>
2021-11-16 01:14:06 +0200

    spring-rest-web-practice

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.impl.response.UserResponseMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserResponseMapper userMapper;
+    private final UserService userService;
+
+    public UserController(UserService userService, UserResponseMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        User user = userService.findByEmail(email)
+                    .orElseThrow(() -> new RuntimeException("Invalid email"));
+        return userMapper.toDto(user);
+    }
+}
