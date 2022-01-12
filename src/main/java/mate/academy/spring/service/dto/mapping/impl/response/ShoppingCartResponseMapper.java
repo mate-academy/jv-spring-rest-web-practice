f01e0c6402072c4c723f915de1b976599d7b83fc
Olha Tankevych <freakystyley85@gmail.com>
2022-01-12 16:14:21 +0200

    Implemented OrderController, AuthenticationController, UserController, ShoppingCartController

---
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/ShoppingCartResponseMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.dto.mapping.impl.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto,
+        ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart shoppingCart) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setUserId(shoppingCart.getUser().getId());
+        List<Long> ticketsId = shoppingCart.getTickets().stream()
+                        .map(Ticket::getId)
+                        .collect(Collectors.toList());
+        shoppingCartResponseDto.setTicketsId(ticketsId);
+        return shoppingCartResponseDto;
+    }
+}
