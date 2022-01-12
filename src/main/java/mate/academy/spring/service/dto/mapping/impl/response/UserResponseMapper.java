74393ddc7dd1776b0aabbacb46e7d7ba4ed94c62
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-12 11:37:32 +0200

    From missing annotation, remove empty lines

--- src/main/java/mate/academy/spring/service/dto/mapping/impl/response/UserResponseMapper.java
+++ src/main/java/mate/academy/spring/service/dto/mapping/impl/response/UserResponseMapper.java
@@ -3,7 +3,9 @@ package mate.academy.spring.service.dto.mapping.impl.response;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
     @Override
     public UserResponseDto toDto(User user) {
@@ -11,6 +13,5 @@ public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, Us
                 .id(user.getId())
                 .email(user.getEmail())
                 .build();
-
     }
 }
