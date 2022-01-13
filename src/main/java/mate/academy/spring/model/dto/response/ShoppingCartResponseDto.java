d3e4c5d216b411d827d891147efea7553e4bac48
Dzmitry Kozel <dmitrokoz88@gmail.com>
2022-01-13 23:07:39 +0200

    BUILD SUCCESS;

---
+++ src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.model.dto.response;
+
+import java.util.List;
+
+public class ShoppingCartResponseDto {
+    private Long id;
+    private List<Long> ticketIds;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Long> getTicketIds() {
+        return ticketIds;
+    }
+
+    public void setTicketIds(List<Long> ticketsIds) {
+        this.ticketIds = ticketsIds;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
