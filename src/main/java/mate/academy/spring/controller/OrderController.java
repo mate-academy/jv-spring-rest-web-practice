7f659d672b3cda0282fad2cc8182a7e4a160c95d
Oleksii-kho <oleksiikhoriev@gmail.com>
2022-01-14 02:58:44 +0200

    my solution

---
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final DtoResponseMapper<OrderResponseDto, Order> orderResponseMapper;
+    private final OrderService orderService;
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+
+    public OrderController(DtoResponseMapper<OrderResponseDto, Order> orderResponseMapper,
+                           OrderService orderService, ShoppingCartService shoppingCartService,
+                           UserService userService) {
+        this.orderResponseMapper = orderResponseMapper;
+        this.orderService = orderService;
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/complete")
+    public OrderResponseDto completeOrder(@RequestParam Long userId) {
+        ShoppingCart shoppingCart = shoppingCartService.getByUser(userService.get(userId));
+        return orderResponseMapper.toDto(orderService.completeOrder(shoppingCart));
+    }
+
+    @GetMapping
+    public List<OrderResponseDto> getOrdersHistory(@RequestParam Long userId) {
+        return orderService.getOrdersHistory(userService.get(userId)).stream()
+                .map(orderResponseMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
