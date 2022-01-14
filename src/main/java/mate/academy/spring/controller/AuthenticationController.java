131a8b3626bf18f65b450fb727698e812ccc4707
farfardesu <savavlad003@gmail.com>
2022-01-14 12:41:59 +0200

    suggested changes were implemented

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping()
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final DtoResponseMapper<UserResponseDto, User> responseMapper;
 
-    public AuthenticationController(AuthenticationService authenticationService) {
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    DtoResponseMapper<UserResponseDto, User> responseMapper) {
         this.authenticationService = authenticationService;
+        this.responseMapper = responseMapper;
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody UserRequestDto requestDto) {
-        authenticationService.register(requestDto.getEmail(), requestDto.getPassword());
+    public UserResponseDto register(@RequestBody UserRequestDto requestDto) {
+        return responseMapper.toDto(authenticationService.register(requestDto.getEmail(),
+                requestDto.getPassword()));
     }
 }
