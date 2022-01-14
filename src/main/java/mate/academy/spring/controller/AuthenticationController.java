fec7322f70be033add0f35cb4fc3d986a99f6067
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-14 14:47:17 +0200

    redunt import was deleted

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -3,7 +3,6 @@ package mate.academy.spring.controller;
 import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
