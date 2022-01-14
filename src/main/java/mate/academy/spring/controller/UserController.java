77ff78f2afb3f14a09ba5a59fd89ad73ccadddb1
Oleg Shiriaev <mystidreamer@gmail.com>
2022-01-14 13:01:17 +0200

    spring rest web app with authentication was implemented

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.dto.mapping.DtoResponseMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final DtoResponseMapper<UserResponseDto, User> dtoResponseMapper;
+    private final UserService userService;
+
+    public UserController(DtoResponseMapper<UserResponseDto,
+            User> dtoResponseMapper, UserService userService) {
+        this.dtoResponseMapper = dtoResponseMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        User user = userService.findByEmail(email).orElseThrow(() ->
+                new RuntimeException("Can't find user by email: " + email));
+        return dtoResponseMapper.toDto(user);
+    }
+}
