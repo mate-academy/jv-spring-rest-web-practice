5ca18776c95ae0e59ab80ea4371aea406a14b430
Yaroslav Boiko <slkawnia@gmail.com>
2022-03-29 19:26:19 +0300

    Implemantation of HW Rest Web Practice

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -0,0 +1,19 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserResponseMapper implements
+        DtoResponseMapper<UserResponseDto, User> {
+
+    @Override
+    public UserResponseDto toDto(User user) {
+        UserResponseDto authenticationResponseDto = new UserResponseDto();
+        authenticationResponseDto.setId(user.getId());
+        authenticationResponseDto.setEmail(user.getEmail());
+        return authenticationResponseDto;
+    }
+}
