ae7d44fcb29cba98a563785bc0b188c6e67999a7
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 19:06:59 +0300

    Add AuthenticationService, AuthenticationServiceImpl

---
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.service.impl;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final UserService userService;
+
+    public AuthenticationServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
