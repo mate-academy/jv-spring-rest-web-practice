c27987a9f1ccf2d0f9ecef5fe9f7cb0b45166a37
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 19:22:29 +0300

    Add AuthenticationController

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseDtoMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserResponseDtoMapper userResponseDtoMapper;
+
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    UserResponseDtoMapper userResponseDtoMapper) {
+        this.authenticationService = authenticationService;
+        this.userResponseDtoMapper = userResponseDtoMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+        User registered = authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
+        return userResponseDtoMapper.toDto(registered);
+    }
+}
