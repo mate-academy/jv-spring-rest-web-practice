0088b7246b7d51f77cbb4eaa8dddd5dd931e3729
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 20:05:16 +0300

    Fix AuthenticationServiceImpl

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -3,15 +3,19 @@ package mate.academy.spring.service.impl;
 import mate.academy.spring.exception.AuthenticationException;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
+    private final ShoppingCartService shoppingCartService;
 
-    public AuthenticationServiceImpl(UserService userService) {
+    public AuthenticationServiceImpl(UserService userService,
+                                     ShoppingCartService shoppingCartService) {
         this.userService = userService;
+        this.shoppingCartService = shoppingCartService;
     }
 
     @Override
@@ -23,6 +27,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
-        return userService.add(user);
+        user = userService.add(user);
+        shoppingCartService.registerNewShoppingCart(user);
+        return user;
     }
 }
