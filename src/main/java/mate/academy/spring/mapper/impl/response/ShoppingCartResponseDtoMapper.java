6fb7617e2e56f07ea6e399856aff193d29af7fae
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 20:22:47 +0300

    Add ShoppingCartResponseDtoMapper

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class ShoppingCartResponseDtoMapper
+        implements DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart object) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setId(object.getId());
+        shoppingCartResponseDto.setUserId(object.getUser().getId());
+        shoppingCartResponseDto.setTicketIds(object.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        return shoppingCartResponseDto;
+    }
+}
