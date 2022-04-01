65495cfe7963511e87dede2bc9d4b67f0a9b2e0f
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 19:46:11 +0300

    Add UserController

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseDtoMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final UserResponseDtoMapper userResponseDtoMapper;
+
+    public UserController(UserService userService,
+                          UserResponseDtoMapper userResponseDtoMapper) {
+        this.userService = userService;
+        this.userResponseDtoMapper = userResponseDtoMapper;
+    }
+
+    @GetMapping("/users/by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        Optional<User> byEmail = userService.findByEmail(email);
+        User user = byEmail.orElseThrow(() -> new RuntimeException("No user with email: " + email));
+        return userResponseDtoMapper.toDto(user);
+    }
+}
