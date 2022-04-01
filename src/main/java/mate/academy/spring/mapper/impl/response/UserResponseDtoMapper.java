9e0bfc3af4b3adac8d1eeb6d70950c4d15bfbf9b
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-01 19:17:42 +0300

    Add UserResponseDtoMapper

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseDtoMapper.java
@@ -0,0 +1,15 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+
+public class UserResponseDtoMapper implements DtoResponseMapper<UserResponseDto, User> {
+    @Override
+    public UserResponseDto toDto(User object) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(object.getId());
+        userResponseDto.setEmail(object.getEmail());
+        return userResponseDto;
+    }
+}
