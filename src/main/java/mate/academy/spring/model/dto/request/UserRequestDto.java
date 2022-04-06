71bb388dea6c97b3f1b744435eae4611e1c1f3f2
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-06 14:38:48 +0300

    added UserRequestDto and AuthenticationController

---
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.model.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class UserRequestDto {
+    @NotNull
+    private String email;
+    @NotNull
+    @Min(4)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
