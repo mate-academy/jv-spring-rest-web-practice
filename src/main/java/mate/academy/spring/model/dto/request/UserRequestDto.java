72c3dbd242d6dc81b0ba009ec1ba4e8e61954e3c
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-06 15:55:57 +0300

    added lombok configuration and annotations

--- src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -2,27 +2,15 @@ package mate.academy.spring.model.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserRequestDto {
     @NotNull
     private String email;
     @NotNull
     @Min(4)
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -3,43 +3,15 @@ package mate.academy.spring.model.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class OrderResponseDto {
     private Long id;
     private List<Long> ticketIds;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime orderDate;
     private Long userId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Long> getTicketIds() {
-        return ticketIds;
-    }
-
-    public void setTicketIds(List<Long> ticketIds) {
-        this.ticketIds = ticketIds;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
