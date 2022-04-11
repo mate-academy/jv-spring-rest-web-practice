588ce4b9a6c5afbe7e8373690b2684036b6f00aa
Vadym <vadya829@gmail.com>
2022-04-11 18:43:02 +0200

    ShoppingCartController Put and Get methods support was implemented

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart object) {
+        ShoppingCartResponseDto response = new ShoppingCartResponseDto();
+        response.setId(object.getId());
+        object.getTickets()
+                .stream()
+                .map(Ticket::getId)
+                .forEach(response.getTicketIds()::add);
+        response.setUserId(object.getUser().getId());
+        return response;
+    }
+}
