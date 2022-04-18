f7a98ac7bede2c626b5af396ec53a4598bdcf528
anyutamysko <a_misko@ukr.net>
2022-04-18 23:37:36 +0300

    impl web practice

---
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -0,0 +1,49 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final OrderService orderService;
+    private final OrderResponseMapper orderResponseMapper;
+
+    public OrderController(ShoppingCartService shoppingCartService,
+                           UserService userService, OrderService orderService,
+                           OrderResponseMapper orderResponseMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.orderService = orderService;
+        this.orderResponseMapper = orderResponseMapper;
+    }
+
+    @PostMapping("/complete")
+    public OrderResponseDto completeOrder(@RequestParam Long userId) {
+        ShoppingCart shoppingCart = shoppingCartService.getByUser(userService.get(userId));
+        return orderResponseMapper.toDto(orderService.completeOrder(shoppingCart));
+    }
+
+    @GetMapping
+    public List<OrderResponseDto> getOrderHistory(@RequestParam Long userId) {
+        List<Order> ordersHistory = orderService.getOrdersHistory(userService.get(userId));
+        return ordersHistory
+                .stream()
+                .map(orderResponseMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class UserRequestDto {
+    @Email
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    @Size(min = 6)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
