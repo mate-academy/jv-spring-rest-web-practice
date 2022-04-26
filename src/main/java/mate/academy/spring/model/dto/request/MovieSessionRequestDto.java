99a16fe6cb5cde238495e1e30949228202b464a1
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-04-26 20:05:05 +0300

    AuthController + OrderController were implemented

--- src/main/java/mate/academy/spring/model/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/MovieSessionRequestDto.java
@@ -1,9 +1,11 @@
 package mate.academy.spring.model.dto.request;
 
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class MovieSessionRequestDto {
     private Long movieId;
+    @JsonFormat(pattern = "dd.MM.yyyy HH:mm")
     private LocalDateTime showTime;
     private Long cinemaHallId;
 
