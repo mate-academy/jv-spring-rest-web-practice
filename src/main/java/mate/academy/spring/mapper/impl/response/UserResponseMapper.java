1e20144742b71975a370cbcfae2c2c59a9c84876
sltnsrh <lifenjoy8888@gmail.com>
2022-04-27 15:59:05 +0300

    web practice implementation

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
+    @Override
+    public UserResponseDto toDto(User object) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(object.getId());
+        userResponseDto.setEmail(object.getEmail());
+        return userResponseDto;
+    }
+}
