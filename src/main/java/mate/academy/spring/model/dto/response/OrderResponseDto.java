cc7c1351d5bf5b0db3bc8b651990016f6880c04e
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-27 17:58:31 +0300

    order response dto improvement

--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -1,11 +1,13 @@
 package mate.academy.spring.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class OrderResponseDto {
     private Long id;
     private List<Long> ticketsId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime orderDate;
     private Long userId;
 
