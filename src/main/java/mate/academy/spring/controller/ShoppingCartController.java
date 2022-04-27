79f7316b803f8d880f21f44f19003d519a57d2dd
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-04-27 16:15:58 +0300

    Checklist fixes

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,6 +1,6 @@
 package mate.academy.spring.controller;
 
-import mate.academy.spring.mapper.impl.response.ShoppingCartResponseMapper;
+import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.User;
@@ -9,7 +9,7 @@ import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,19 +20,21 @@ public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
     private final MovieSessionService movieSessionService;
     private final UserService userService;
-    private final ShoppingCartResponseMapper shoppingCartResponseMapper;
+    private final DtoResponseMapper<ShoppingCartResponseDto,
+            ShoppingCart> shoppingCartDtoResponseMapper;
 
     public ShoppingCartController(ShoppingCartService shoppingCartService,
                                   MovieSessionService movieSessionService,
                                   UserService userService,
-                                  ShoppingCartResponseMapper shoppingCartResponseMapper) {
+                                  DtoResponseMapper<ShoppingCartResponseDto,
+                                          ShoppingCart> shoppingCartDtoResponseMapper) {
         this.shoppingCartService = shoppingCartService;
         this.movieSessionService = movieSessionService;
         this.userService = userService;
-        this.shoppingCartResponseMapper = shoppingCartResponseMapper;
+        this.shoppingCartDtoResponseMapper = shoppingCartDtoResponseMapper;
     }
 
-    @PostMapping("/movie-sessions")
+    @PutMapping("/movie-sessions")
     public void addSession(@RequestParam Long userId, @RequestParam Long movieSessionId) {
         User user = userService.get(userId);
         MovieSession session = movieSessionService.get(movieSessionId);
@@ -43,6 +45,6 @@ public class ShoppingCartController {
     public ShoppingCartResponseDto getByUserId(@RequestParam Long userId) {
         User user = userService.get(userId);
         ShoppingCart cart = shoppingCartService.getByUser(user);
-        return shoppingCartResponseMapper.toDto(cart);
+        return shoppingCartDtoResponseMapper.toDto(cart);
     }
 }
