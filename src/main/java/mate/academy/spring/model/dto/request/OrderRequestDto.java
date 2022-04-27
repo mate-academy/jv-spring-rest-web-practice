3de4bda4380b89652b56a7ed809442fa03979af5
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-27 13:31:17 +0300

    order controller, mapper and dto was created

---
+++ src/main/java/mate/academy/spring/model/dto/request/OrderRequestDto.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.model.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderRequestDto {
+    private List<Long> ticketsId;
+    private LocalDateTime orderDate;
+    private Long userId;
+
+    public List<Long> getTicketsId() {
+        return ticketsId;
+    }
+
+    public void setTicketsId(List<Long> ticketsId) {
+        this.ticketsId = ticketsId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long user) {
+        this.userId = user;
+    }
+}
