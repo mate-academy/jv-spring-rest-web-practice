c47c5f2eff301813a37077018a1c5c9ef81b0ef4
sltnsrh <lifenjoy8888@gmail.com>
2022-04-27 17:00:50 +0300

    annotation email was added

--- src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -1,10 +1,12 @@
 package mate.academy.spring.model.dto.request;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserRequestDto {
     @NotNull
+    @Email
     private String email;
     @NotNull
     @Size(min = 6)
