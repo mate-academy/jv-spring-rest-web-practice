217a9d0d65cea4e0e5402b6cf516b8b2b689a201
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-05-09 12:09:59 +0300

    HW-s4. Spring REST practice.

---
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderResponseDto {
+    private Long orderId;
+    private UserResponseDto userResponseDto;
+    private List<Long> ticketsId;
+    private LocalDateTime orderDateTime;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<Long> getTicketsId() {
+        return ticketsId;
+    }
+
+    public void setTicketsId(List<Long> ticketsId) {
+        this.ticketsId = ticketsId;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public void setOrderDateTime(LocalDateTime orderDateTime) {
+        this.orderDateTime = orderDateTime;
+    }
+
+    public UserResponseDto getUserResponseDto() {
+        return userResponseDto;
+    }
+
+    public void setUserResponseDto(UserResponseDto userResponseDto) {
+        this.userResponseDto = userResponseDto;
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.model.dto.response;
+
+import java.util.List;
+
+public class ShoppingCartResponseDto {
+    private Long shoppingCartId;
+    private String userEmail;
+    private List<Long> ticketsId;
+
+    public Long getShoppingCartId() {
+        return shoppingCartId;
+    }
+
+    public void setShoppingCartId(Long shoppingCartId) {
+        this.shoppingCartId = shoppingCartId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public List<Long> getTicketsId() {
+        return ticketsId;
+    }
+
+    public void setTicketsId(List<Long> ticketsId) {
+        this.ticketsId = ticketsId;
+    }
+}
