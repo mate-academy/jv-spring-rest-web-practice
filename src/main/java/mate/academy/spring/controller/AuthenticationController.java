21737ec9d51ae46385a9b88b02666e595e7822ac
Viacheslav Cheban <vcheb@eg.dk>
2022-05-15 16:51:37 +0300

    fix code style

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -4,9 +4,11 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class AuthenticationController {
     private final UserService userService;
 
