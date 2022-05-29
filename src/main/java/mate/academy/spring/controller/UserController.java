85ff10fc3257f2a31d0fa905485459b330e14716
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2022-05-29 16:24:03 +0300

    Solution of the task

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private DtoResponseMapper<UserResponseDto, User> userResponseDtoMapper;
+    private UserService userService;
+
+    @Autowired
+    public UserController(DtoResponseMapper<UserResponseDto, User> userResponseDtoMapper,
+                          UserService userService) {
+        this.userResponseDtoMapper = userResponseDtoMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto findByEmail(@RequestParam String email) {
+        return userResponseDtoMapper.toDto(userService.findByEmail(email).get());
+    }
+}
