d2f23b2b6aebc7b5bd0aea480ebbd4a705d9b178
Roman Kachmaryk <roman.kv98@gmail.com>
2022-06-20 15:07:50 +0300

    new controllers, mappers and dto-classes have been created

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper
+        implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setOrderDate(order.getOrderDate());
+        orderResponseDto.setTicketsIds(parseTickets(order.getTickets()));
+        return orderResponseDto;
+    }
+
+    private List<Long> parseTickets(List<Ticket> tickets) {
+        return tickets.stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList());
+    }
+}
