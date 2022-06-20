82af73de8daa612119935c7c62b0228eaf2e659a
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-06-20 12:43:14 +0300

    Added @Component annotation

--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseDtoMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseDtoMapper.java
@@ -5,7 +5,9 @@ import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ShoppingCartResponseDtoMapper implements
         DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
     @Override
