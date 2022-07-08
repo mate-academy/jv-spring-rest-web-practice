2277596ebd5c0f8b83c461d079065e88151e316a
daniel <daniya522@gmail.com>
2022-07-08 12:59:39 +0300

    added new description for getUserByEmail exception.

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -24,6 +24,6 @@ public class UserController {
     @GetMapping("/by-email")
     public UserResponseDto getUserByEmail(@RequestParam String email) {
         return responseMapper.toDto(userService.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Please, enter your email.")));
+                .orElseThrow(() -> new RuntimeException("Can't find user by email: " + email)));
     }
 }
