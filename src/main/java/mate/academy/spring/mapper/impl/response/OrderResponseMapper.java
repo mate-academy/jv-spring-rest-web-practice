ff336c29ace99afa910321461855f70609a2d8c7
Andrey Chumak <chumakandrey7@gmail.com>
2022-08-03 23:09:59 +0300

    - Created controllers: Order, Authentication, User, ShoppingCart - Created DTO and DTO mappers

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.ArrayList;
+import java.util.List;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setOrderDate(order.getOrderDate());
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setUserId(order.getUser().getId());
+        List<Long> ticketsId = new ArrayList<>();
+        for (Ticket ticket : order.getTickets()) {
+            ticketsId.add(ticket.getId());
+        }
+        orderResponseDto.setTicketsId(ticketsId);
+        return orderResponseDto;
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRequestDto {
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+    @NotNull
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
