9932e16674181786ed6673416cd7dea02733a081
Maksym Karamavrov <makcumkaka@gmail.com>
2022-08-07 18:26:03 +0300

    First commit. Application runs clear.

---
+++ src/main/java/mate/academy/spring/model/dto/request/ShoppingCartRequestDto.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.model.dto.request;
+
+import java.util.List;
+
+public class ShoppingCartRequestDto {
+    private List<Long> ticketsId;
+
+    private Long userId;
+
+    public List<Long> getTicketsId() {
+        return ticketsId;
+    }
+
+    public void setTicketsId(List<Long> ticketsId) {
+        this.ticketsId = ticketsId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
