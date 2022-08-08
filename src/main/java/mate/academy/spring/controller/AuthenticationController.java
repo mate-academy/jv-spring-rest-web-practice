12aae03e33f2d6a232eb8cca85d3926ffbd440de
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-08-09 00:23:22 +0300

    hw-spring-rest-web-practice

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final DtoResponseMapper<UserResponseDto, User> responseMapper;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(DtoResponseMapper<UserResponseDto, User> responseMapper,
+                                    AuthenticationService authenticationService) {
+        this.responseMapper = responseMapper;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto registration(@RequestBody UserRequestDto requestDto) {
+        return responseMapper.toDto(authenticationService
+                .register(requestDto.getEmail(), requestDto.getPassword()));
+    }
+}
