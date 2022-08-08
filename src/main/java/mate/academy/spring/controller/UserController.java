117c4f4a01e116066309eb3f978a9d31177dd957
Dima Podakin <calombo.podakin@ukr.net>
2022-08-08 23:48:08 +0300

    created spring-rest-practice task

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserResponseMapper userResponseMapper;
+    private final UserService userService;
+
+    public UserController(UserResponseMapper userResponseMapper,
+                          UserService userService) {
+        this.userResponseMapper = userResponseMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        return userResponseMapper.toDto(userService.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Cannot find user by email: "
+                        + email)));
+    }
+}
