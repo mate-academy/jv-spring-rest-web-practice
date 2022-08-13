bf28e6d3a14f2b720fac47760911f303e5b5457c
Oleksandr Domnich <45822497+dmnichz@users.noreply.github.com>
2022-08-13 16:10:54 +0300

    Update UserResponseDto.java

--- src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.model.dto.response;
 
 public class UserResponseDto {
+    private Long id;
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
