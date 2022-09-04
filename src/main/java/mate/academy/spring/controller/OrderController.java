4dd24f53fd16d6366b6ff032f1f19f86b6318565
drapail <mv.dibrov@gmail.com>
2022-09-04 11:43:46 +0300

    Authentication and User controllers added

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -1,24 +1,23 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.spring.dao.TicketDao;
 import mate.academy.spring.mapper.impl.request.OrderRequestMapper;
 import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.ShoppingCart;
-import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.User;
-import mate.academy.spring.model.dto.request.OrderRequestDto;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.service.impl.ShoppingCartServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.AuthenticationResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private final AuthenticationResponseMapper authenticationResponseMapper;
+
+    public UserController(UserService userService, AuthenticationResponseMapper authenticationResponseMapper) {
+        this.userService = userService;
+        this.authenticationResponseMapper = authenticationResponseMapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getUserByEmail(@RequestParam String email) {
+        User user = userService.findByEmail(email).get();
+        return authenticationResponseMapper.toDto(user);
+    }
+}
