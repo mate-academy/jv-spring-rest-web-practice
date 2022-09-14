3833e03dbf6abb009b208397b1b8de597cbb3e7c
l_osyash <los.nazar24@gmail.com>
2022-09-14 21:13:20 +0300

    Added fixes.

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,10 +1,7 @@
 package mate.academy.spring.controller;
 
 import jakarta.validation.Valid;
-import mate.academy.spring.mapper.DtoResponseMapper;
-import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
-import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
-    private final DtoResponseMapper<UserResponseDto, User> dtoResponseMapper;
 
-    public AuthenticationController(AuthenticationService authenticationService,
-                                    DtoResponseMapper<UserResponseDto, User> dtoResponseMapper) {
+    public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
-        this.dtoResponseMapper = dtoResponseMapper;
     }
 
     @PostMapping
     @RequestMapping("/register")
-    public UserResponseDto register(@RequestBody @Valid UserRequestDto userRequestDto) {
-        User registeredUser = authenticationService
-                .register(userRequestDto.getEmail(), userRequestDto.getPassword());
-        return dtoResponseMapper.toDto(registeredUser);
+    public void register(@RequestBody @Valid UserRequestDto userRequestDto) {
+        authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
     }
 }
--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -11,7 +11,6 @@ import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,8 +41,8 @@ public class OrderController {
         return responseMapper.toDto(order);
     }
 
-    @GetMapping("/{userId}")
-    public List<OrderResponseDto> getOrdersHistory(@PathVariable Long userId) {
+    @GetMapping
+    public List<OrderResponseDto> getOrdersHistory(@RequestParam Long userId) {
         User user = userService.get(userId);
         return orderService.getOrdersHistory(user).stream()
                 .map(responseMapper::toDto)
