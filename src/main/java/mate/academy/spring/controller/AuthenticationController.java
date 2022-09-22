0cc1c2c617b830e75cc25c0996c4875b9cb3517a
agash <alexgash8@gmail.com>
2022-09-22 15:26:55 +0300

    mvn clean package as always

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -2,7 +2,6 @@ package mate.academy.spring.controller;
 
 import mate.academy.spring.exception.AuthenticationException;
 import mate.academy.spring.mapper.DtoResponseMapper;
-import mate.academy.spring.mapper.impl.response.UserResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.model.dto.response.UserResponseDto;
