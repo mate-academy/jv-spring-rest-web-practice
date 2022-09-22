8ee975631544f3d78b7d732981e5bd1822db8da6
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-22 21:38:08 +0300

    Removed unnecessary userId from ShoppingCartResponseDto

--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -14,7 +14,6 @@ public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCar
     public ShoppingCartResponseDto toDto(ShoppingCart shoppingCart) {
         ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
         shoppingCartResponseDto.setId(shoppingCart.getId());
-        shoppingCartResponseDto.setUserId(shoppingCart.getId());
         shoppingCartResponseDto.setTicketIds(shoppingCart.getTickets().stream()
                 .map(Ticket::getId)
                 .collect(Collectors.toList()));
