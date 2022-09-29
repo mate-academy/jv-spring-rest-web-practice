cb22c818f48c2fdaa75cd2e5b57e491af65b128b
Nazar <litvik.12@gmail.com>
2022-09-30 01:01:47 +0300

    added Order/ShoppingCart/User and Authentication Controllers

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private final UserService userService;
+    private final UserResponseMapper mapper;
+
+    public UserController(UserService userService, UserResponseMapper mapper) {
+        this.userService = userService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto findUserByEmail(@RequestParam String email) {
+        return userService.findByEmail(email).map(mapper::toDto).get();
+    }
+}
