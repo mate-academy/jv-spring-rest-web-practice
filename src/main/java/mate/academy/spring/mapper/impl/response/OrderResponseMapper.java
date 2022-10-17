294d4b7a61e7162c5142f8c041e8fbbdb1acd744
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-18 01:16:10 +0200

    add OrderResponseDto

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -3,9 +3,6 @@ package mate.academy.spring.mapper.impl.response;
 import mate.academy.spring.mapper.DtoRequestMapper;
 import mate.academy.spring.model.Order;
 
-public class OrderResponseMapper implements DtoRequestMapper {
-    @Override
-    public OrderResponseDto fromDto(Order order) {
+public class OrderResponseMapper {
 
-    }
 }
---
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderResponseDto {
+    Long id;
+    List<Long> ticketsId;
+    LocalDateTime localDateTime;
+    Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Long> getTicketsId() {
+        return ticketsId;
+    }
+
+    public void setTicketsId(List<Long> ticketsId) {
+        this.ticketsId = ticketsId;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
