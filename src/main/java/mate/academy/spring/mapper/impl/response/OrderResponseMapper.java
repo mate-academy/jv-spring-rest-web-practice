1c06140f5b6c280054d2aa29d21a7064db48cd53
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-18 01:25:01 +0200

    implement toDto()

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -1,8 +1,25 @@
 package mate.academy.spring.mapper.impl.response;
 
-import mate.academy.spring.mapper.DtoRequestMapper;
+import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
 
-public class OrderResponseMapper {
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto responseDto = new OrderResponseDto();
+        responseDto.setId(order.getId());
+        responseDto.setTicketsId(order.getTickets().stream()
+                .map(ticket -> ticket.getId())
+                .collect(Collectors.toList()));
+        responseDto.setLocalDateTime(order.getOrderDate());
+        responseDto.setUserId(order.getUser().getId());
+        return responseDto;
+    }
 }
