39bfb2b42e6d18ce4bdaef0e03c722c7c8d335c0
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-18 01:14:53 +0200

    add OrderRequestDto

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,11 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoRequestMapper;
+import mate.academy.spring.model.Order;
+
+public class OrderResponseMapper implements DtoRequestMapper {
+    @Override
+    public OrderResponseDto fromDto(Order order) {
+
+    }
+}
