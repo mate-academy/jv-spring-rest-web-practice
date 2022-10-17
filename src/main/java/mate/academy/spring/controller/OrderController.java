f888d07a23f8a445152d218c7f837d65106773d6
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-18 00:05:19 +0200

    add OrderController

---
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+}
