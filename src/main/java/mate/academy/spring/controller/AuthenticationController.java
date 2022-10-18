22846238dd06c0883dca11dd002572771a4c3585
Serhii Buria <sergeiburya@gmail.com>
2022-10-18 20:46:03 +0300

    fixed AuthenticationController

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,10 +1,7 @@
 package mate.academy.spring.controller;
 
 import jakarta.validation.Valid;
-import mate.academy.spring.mapper.DtoResponseMapper;
-import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
-import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
