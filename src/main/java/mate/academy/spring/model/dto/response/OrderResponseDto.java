336a47333d74057584f6d569ccf2a2978625ba36
Serhii Buria <sergeiburya@gmail.com>
2022-10-18 17:18:35 +0300

    added response dto

--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -1,4 +1,43 @@
 package mate.academy.spring.model.dto.response;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class OrderResponseDto {
+    private Long orderId;
+    private List<Long> ticketIds;
+    private LocalDateTime orderDate;
+    private Long userId;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<Long> getTicketIds() {
+        return ticketIds;
+    }
+
+    public void setTicketIds(List<Long> ticketIds) {
+        this.ticketIds = ticketIds;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
--- src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
@@ -1,4 +1,22 @@
 package mate.academy.spring.model.dto.response;
 
 public class UserResponseDto {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private Long userId;
+    private String email;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
