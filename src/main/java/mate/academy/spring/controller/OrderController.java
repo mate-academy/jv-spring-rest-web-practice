be356657d597cba8bbd9da9434cdd0c75252743c
max-bolotin <realprof.mx@gmail.com>
2022-10-19 11:19:31 +0300

    redundant field removed, mapping in OrderController class corrected

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
