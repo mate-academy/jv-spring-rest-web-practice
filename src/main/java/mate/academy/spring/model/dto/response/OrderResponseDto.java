9491edfc03db78c2b9120fa935cdf0451df22975
LubusUzh <lubus.uzh@gmail.com>
2022-10-20 00:44:57 +0300

    Add: lombok dependency to the pom.xml and annotations to the OrderResponseDto instead of getters and setters

--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -2,42 +2,13 @@ package mate.academy.spring.model.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class OrderResponseDto {
     private Long id;
     private List<Long> ticketsId;
     private LocalDateTime orderDate;
     private Long userId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Long> getTicketsId() {
-        return ticketsId;
-    }
-
-    public void setTicketsId(List<Long> ticketsId) {
-        this.ticketsId = ticketsId;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
