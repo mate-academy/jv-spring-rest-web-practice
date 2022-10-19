77467eab5a30881b681e7850a66ea40742cd8811
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-19 12:31:56 +0200

    implment UserController

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    private UserResponseMapper userResponseMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserResponseMapper userResponseMapper) {
+        this.userService = userService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @GetMapping("by-email")
+    public UserResponseDto getUserByEmail(@RequestParam String email) {
+        Optional<User> userByEmail = userService.findByEmail(email);
+        if (userByEmail.isPresent()) {
+            return userResponseMapper.toDto(userByEmail.get());
+        }
+        throw new RuntimeException("Can`t get user by email " + email);
+    }
+}
