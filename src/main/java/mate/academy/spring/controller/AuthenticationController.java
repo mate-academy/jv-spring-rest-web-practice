0248891527acac9d4515350946ab4d4394f22403
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-19 08:45:12 +0200

    implement AuthenticationController

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,10 +1,30 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
     private AuthenticationService authenticationService;
-    private 
+    private UserResponseMapper userResponseMapper;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService, UserResponseMapper userResponseMapper) {
+        this.authenticationService = authenticationService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+        User user = authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
+        return userResponseMapper.toDto(user);
+    }
 }
---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.mapper.impl.response;
+
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+
+public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
+    @Override
+    public UserResponseDto toDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setPassword(user.getPassword());
+        return userResponseDto;
+    }
+}
