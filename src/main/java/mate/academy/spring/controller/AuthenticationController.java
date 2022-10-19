edf4dfbf0e4e63457beecb68d122755bd969b870
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-19 08:30:25 +0200

    add UserDto

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private AuthenticationService authenticationService;
+    private 
+}
---
+++ src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.model.dto.response;
+
+import com.sun.istack.NotNull;
+
+public class UserResponseDto {
+    private Long id;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
