9c496606daecf74c5c64378d81292d561c934696
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-20 00:33:02 +0300

    add AuthenticationController

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.register.UserRegisterDto;
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class AuthenticationController {
+    UserService userService;
+    UserResponseMapper userResponseMapper;
+
+    public AuthenticationController(UserService userService,
+            UserResponseMapper userResponseMapper) {
+        this.userService = userService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @PostMapping
+    public UserResponseDto register(@RequestBody UserRegisterDto userRegisterDto) {
+        User user = new User();
+        user.setEmail(userRegisterDto.getEmail());
+        user.setPassword(userRegisterDto.getPassword());
+        return userResponseMapper.toDto(userService.add(user));
+    }
+}
--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -9,15 +9,27 @@ import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/orders")
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
     OrderService orderService;
     ShoppingCartService shoppingCartService;
     UserService userService;
     OrderResponseMapper orderResponseMapper;
+
+    public OrderController(OrderService orderService,
+            ShoppingCartService shoppingCartService,
+            UserService userService,
+            OrderResponseMapper orderResponseMapper) {
+        this.orderService = orderService;
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.orderResponseMapper = orderResponseMapper;
+    }
 
     @PostMapping("/complete")
     public OrderResponseDto add(@RequestParam Long userId) {
@@ -32,6 +44,5 @@ public class OrderController {
         return orderService.getOrdersHistory(userService.get(userId)).stream()
                 .map(orderResponseMapper::toDto)
                 .collect(Collectors.toList());
-
     }
 }
