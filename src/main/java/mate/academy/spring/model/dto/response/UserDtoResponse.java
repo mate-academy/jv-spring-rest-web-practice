2ebbef3bb4a7c5638476dd9203188f68782477bb
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-10-20 20:10:54 +0300

    Controllers are created , mappers are implemented, dto are created task is complete

---
+++ src/main/java/mate/academy/spring/model/dto/response/UserDtoResponse.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.model.dto.response;
+
+public class UserDtoResponse {
+
+    private String email;
+    private Long id;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
