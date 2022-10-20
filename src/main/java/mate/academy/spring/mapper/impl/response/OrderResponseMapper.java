fec1a8a26ff627f4b98b639f3ab3044640e37425
Ilja Nekrituk <Nekrit99@gmail.com>
2022-10-20 22:39:53 +0300

    Mappers

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -1,4 +1,22 @@
 package mate.academy.spring.mapper.impl.response;
 
-public class OrderResponseMapper {
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+
+import java.util.stream.Collectors;
+
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order object) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(object.getId());
+        orderResponseDto.setOrderDate(object.getOrderDate());
+        orderResponseDto.setUserId(object.getUser().getId());
+        orderResponseDto.setTicketsId(object.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        return orderResponseDto;
+    }
 }
--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -1,4 +1,21 @@
 package mate.academy.spring.mapper.impl.response;
 
-public class ShoppingCartResponseMapper {
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+
+import java.util.stream.Collectors;
+
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart object) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setId(object.getId());
+        shoppingCartResponseDto.setUserid(object.getUser().getId());
+        shoppingCartResponseDto.setTicketsId(object.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        return shoppingCartResponseDto;
+    }
 }
