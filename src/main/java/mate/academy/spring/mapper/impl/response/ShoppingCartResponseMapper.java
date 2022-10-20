4fb123c6be4375ddbc6ebd62d85e7067a18af614
Vasyl Pankiv <wasilius2@gmail.com>
2022-10-20 09:49:12 +0300

    Implement ShoppingCartController

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
+import java.util.stream.Collectors;
+
+@Component
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto,
+        ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart shoppingCart) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setId(shoppingCart.getId());
+        shoppingCartResponseDto.setTicketsIds(shoppingCart.getTickets()
+                .stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        return shoppingCartResponseDto;
+    }
+}
