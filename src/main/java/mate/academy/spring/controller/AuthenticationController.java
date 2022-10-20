7a3ea1136bee4a15961ec51d856f4988a4b88c7c
LubusUzh <lubus.uzh@gmail.com>
2022-10-20 19:33:17 +0300

    Add: DTO, Mapper and Controller for the User and Authentication Controller

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,22 @@
+package mate.academy.spring.controller;
+
+import lombok.AllArgsConstructor;
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+    private final UserResponseMapper mapper;
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody UserRequestDto dto) {
+        return mapper.toDto(service.register(dto.getEmail(), dto.getPassword()));
+    }
+}
