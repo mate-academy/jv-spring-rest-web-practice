c6596b18dcb4297af5b2602bc90a01898ff09068
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-20 12:29:38 +0300

    remake AuthenticationController

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,30 +1,27 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.mapper.impl.response.UserResponseMapper;
-import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.register.UserRegisterDto;
 import mate.academy.spring.model.dto.response.UserResponseDto;
-import mate.academy.spring.service.UserService;
+import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    private UserService userService;
     private UserResponseMapper userResponseMapper;
+    private AuthenticationService authenticationService;
 
-    public AuthenticationController(UserService userService,
-            UserResponseMapper userResponseMapper) {
-        this.userService = userService;
+    public AuthenticationController(UserResponseMapper userResponseMapper,
+            AuthenticationService authenticationService) {
         this.userResponseMapper = userResponseMapper;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody UserRegisterDto userRegisterDto) {
-        User user = new User();
-        user.setEmail(userRegisterDto.getEmail());
-        user.setPassword(userRegisterDto.getPassword());
-        return userResponseMapper.toDto(userService.add(user));
+        return userResponseMapper.toDto(authenticationService.register(userRegisterDto.getEmail(),
+                userRegisterDto.getPassword()));
     }
 }
