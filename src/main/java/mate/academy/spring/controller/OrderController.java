d4d3167cd4cdf5056a31ab9be302a3c50d5f7081
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-21 19:33:34 +0300

    OrderController, OrderResponseMapper, OrderResponseDto were added

---
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final OrderService orderService;
+    private final OrderResponseMapper orderResponseMapper;
+
+    public OrderController(ShoppingCartService shoppingCartService, UserService userService,
+                           OrderService orderService, OrderResponseMapper orderResponseMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.orderService = orderService;
+        this.orderResponseMapper = orderResponseMapper;
+    }
+
+    @PostMapping("/complete")
+    public OrderResponseDto completeOrder(@RequestParam Long userId) {
+        ShoppingCart shoppingCart = shoppingCartService.getByUser(userService.get(userId));
+        Order order = orderService.completeOrder(shoppingCart);
+        return orderResponseMapper.toDto(order);
+    }
+}
