934a7014f014ce57a6901a6b05e165ba33e19379
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-10-21 15:09:19 +0300

    fixed imports

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,6 +1,7 @@
 package mate.academy.spring.controller;
 
 import jakarta.validation.Valid;
+import java.util.Optional;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
