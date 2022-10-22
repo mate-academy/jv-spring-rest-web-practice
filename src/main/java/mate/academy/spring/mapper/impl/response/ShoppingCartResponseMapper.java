00584d920228b4a42381b858c16ac8f9f0431946
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-22 23:06:50 +0300

    ShoppingCartResponseMapper and ShoppingCartResponseDto were fixed

--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -1,7 +1,10 @@
 package mate.academy.spring.mapper.impl.response;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.dto.request.ShoppingCartResponseDto;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +15,10 @@ public class ShoppingCartResponseMapper
     public ShoppingCartResponseDto toDto(ShoppingCart shoppingCart) {
         ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
         shoppingCartResponseDto.setId(shoppingCart.getId());
-        shoppingCartResponseDto.setTickets(shoppingCart.getTickets());
+        List<Long> ticketsIds = shoppingCart.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList());
+        shoppingCartResponseDto.setTicketIds(ticketsIds);
         shoppingCartResponseDto.setUser(shoppingCartResponseDto.getUser());
         return shoppingCartResponseDto;
     }
