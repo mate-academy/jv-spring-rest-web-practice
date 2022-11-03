4cc84164eaf89adaa57ed9ed01fabeca073ac0ea
Riki Maru <qui.eg0m@gmail.com>
2022-11-03 12:58:19 +0200

    fixed

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,24 +1,27 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.mapper.impl.response.ShoppingCartResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
 @RequestMapping("/shopping-carts")
 public class ShoppingCartController {
     private final UserService userService;
     private final MovieSessionService movieSessionService;
     private final ShoppingCartService shoppingCartService;
-    private final ShoppingCartResponseMapper shoppingCartResponseMapper;
+    private final DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart>
+            shoppingCartResponseMapper;
 
     public ShoppingCartController(UserService userService,
                                   MovieSessionService movieSessionService,
