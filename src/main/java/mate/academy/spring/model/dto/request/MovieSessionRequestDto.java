2d182261498452a5f1c5666aa14ce89a6f79635e
Igor Petrichenko <i.petrichenko@gmail.com>
2022-11-08 10:54:20 +0200

    create more informative methods name

--- src/main/java/mate/academy/spring/model/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/MovieSessionRequestDto.java
@@ -11,23 +11,11 @@ public class MovieSessionRequestDto {
         return movieId;
     }
 
-    public void setMovieId(Long movieId) {
-        this.movieId = movieId;
-    }
-
     public LocalDateTime getShowTime() {
         return showTime;
     }
 
-    public void setShowTime(LocalDateTime showTime) {
-        this.showTime = showTime;
-    }
-
     public Long getCinemaHallId() {
         return cinemaHallId;
-    }
-
-    public void setCinemaHallId(Long cinemaHallId) {
-        this.cinemaHallId = cinemaHallId;
     }
 }
