afa22f488148bcf392d9de2b1bb4a2569cb4e429
Roman Voitalik <voitalik1@gmail.com>
2022-11-14 14:56:16 +0200

    continue

--- src/main/java/mate/academy/spring/mapper/impl/request/ShoppingCartRequestMapperImpl.java
+++ src/main/java/mate/academy/spring/mapper/impl/request/ShoppingCartRequestMapperImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.spring.mapper.impl.request;
 
 import mate.academy.spring.mapper.DtoRequestMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.ShoppingCartRequestDto;
+import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ShoppingCartRequestMapperImpl<D, C> implements DtoRequestMapper<D, C> {
-    private final UserService
+public class ShoppingCartRequestMapperImpl
+        implements DtoRequestMapper<ShoppingCartRequestDto,ShoppingCart> {
+    private final UserService userService;
+    private final ShoppingCartService shoppingCartService;
+
+    public ShoppingCartRequestMapperImpl(UserService userService,
+                                         ShoppingCartService shoppingCartService) {
+        this.userService = userService;
+        this.shoppingCartService = shoppingCartService;
+    }
 
     @Override
-    public C fromDto(D dto) {
-
-        return null;
+    public ShoppingCart fromDto(ShoppingCartRequestDto dto) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        User user = new User();
+        user = userService.get(dto.getUserId());
+        shoppingCart.setUser(user);
+        shoppingCart.setTickets(shoppingCartService.getByUser(user).getTickets());
+        return shoppingCart;
     }
 }
--- src/main/java/mate/academy/spring/model/dto/request/ShoppingCartRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/ShoppingCartRequestDto.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class ShoppingCartRequestDto {
     private List<Long> ticketId;
+    private Long userId;
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
     public List<Long> getTicketId() {
         return ticketId;
@@ -17,6 +25,7 @@ public class ShoppingCartRequestDto {
     public String toString() {
         return "ShoppingCartRequestDto{"
                 + "ticketId=" + ticketId
+                + ", userId=" + userId
                 + '}';
     }
 }
