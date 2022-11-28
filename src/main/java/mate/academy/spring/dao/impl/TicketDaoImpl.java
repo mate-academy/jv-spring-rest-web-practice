aebb3c12124ceea42976768a0fc6b2c856cc578d
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-11-28 22:08:36 +0200

    added get method to TicketDao and TicketService

--- src/main/java/mate/academy/spring/dao/impl/TicketDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/TicketDaoImpl.java
@@ -2,13 +2,27 @@ package mate.academy.spring.dao.impl;
 
 import mate.academy.spring.dao.AbstractDao;
 import mate.academy.spring.dao.TicketDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.Ticket;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public class TicketDaoImpl extends AbstractDao<Ticket> implements TicketDao {
     public TicketDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    @Override
+    public Optional<Ticket> get(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Ticket.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a ticket by id: " + id, e);
+        }
     }
 }
