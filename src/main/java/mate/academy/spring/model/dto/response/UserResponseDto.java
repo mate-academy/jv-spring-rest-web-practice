05dfec86d6688a42af9331d035def14b3908f28e
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-11-30 11:11:56 +0200

    Implemented UserRequestDto validation classes and class AuthenticationController

---
+++ src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.model.dto.response;
+
+import javax.persistence.Column;
+
+public class UserResponseDto {
+    private Long id;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
