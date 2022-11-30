387f00d0aeeb0edafc008d6066471d07d974559e
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-11-30 10:17:32 +0200

    Implemented method getOrderHistory for class OrderController

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -1,10 +1,13 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,5 +35,12 @@ public class OrderController {
                 orderService.completeOrder(
                         shoppingCartService.getByUser(
                                 userService.get(userId))));
+    }
+
+    @GetMapping
+    public List<OrderResponseDto> getOrderHistory(@RequestParam Long userId) {
+        return orderService.getOrdersHistory(userService.get(userId)).stream()
+                .map(orderResponseMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
