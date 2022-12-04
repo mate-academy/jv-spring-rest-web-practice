2495d0230ab92d4d99577c23e98292430b50c7ff
Azarov Artur <azarovartyr@gmail.com>
2022-12-04 11:31:24 +0200

    task is completed

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserResponseMapper userResponseMapper;
+    private final UserService userService;
+
+    public UserController(UserResponseMapper userResponseMapper, UserService userService) {
+        this.userResponseMapper = userResponseMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getUser(@RequestParam String email) {
+        return userResponseMapper.toDto(userService.findByEmail(email).get());
+    }
+}
