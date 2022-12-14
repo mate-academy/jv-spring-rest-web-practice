f44dc27e56e60bb136eb6917c71bba40b40a8fd9
Anatolii Bespalko <tolyan9901@gmail.com>
2022-12-14 19:43:45 +0200

    web-practice

--- src/main/java/mate/academy/spring/model/MovieSession.java
+++ src/main/java/mate/academy/spring/model/MovieSession.java
@@ -8,9 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "movie_sessions")
+@Getter
+@Setter
+@ToString
 public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,46 +26,4 @@ public class MovieSession {
     @ManyToOne(fetch = FetchType.LAZY)
     private CinemaHall cinemaHall;
     private LocalDateTime showTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
-    }
-
-    public CinemaHall getCinemaHall() {
-        return cinemaHall;
-    }
-
-    public void setCinemaHall(CinemaHall cinemaHall) {
-        this.cinemaHall = cinemaHall;
-    }
-
-    public LocalDateTime getShowTime() {
-        return showTime;
-    }
-
-    public void setShowTime(LocalDateTime showTime) {
-        this.showTime = showTime;
-    }
-
-    @Override
-    public String toString() {
-        return "MovieSession{"
-                + "id=" + id
-                + ", movie=" + movie
-                + ", cinemaHall=" + cinemaHall
-                + ", showTime=" + showTime
-                + '}';
-    }
 }
--- src/main/java/mate/academy/spring/model/Order.java
+++ src/main/java/mate/academy/spring/model/Order.java
@@ -10,9 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "orders")
+@Getter
+@Setter
+@ToString
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,46 +28,4 @@ public class Order {
     private LocalDateTime orderDate;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Ticket> getTickets() {
-        return tickets;
-    }
-
-    public void setTickets(List<Ticket> tickets) {
-        this.tickets = tickets;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{"
-                + " id=" + id
-                + ", tickets=" + tickets
-                + ", orderDate=" + orderDate
-                + ", user=" + user
-                + '}';
-    }
 }
