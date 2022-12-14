50ff54f9cc3c8816cf1d1d6bc38407a51a44d18f
galenkoigor <galenkoigor@gmail.com>
2022-12-14 22:14:44 +0100

    Implemented OrderController

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -1,6 +1,14 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private final OrderService orderService;
+    private final ShoppingCartService cartService;
+    private final UserService userService;
+    private final DtoResponseMapper<OrderResponseDto, Order> dtoResponseMapper;
+
+    @Autowired
+    public OrderController(OrderService orderService,
+                           ShoppingCartService cartService,
+                           UserService userService,
+                           DtoResponseMapper<OrderResponseDto, Order> dtoResponseMapper) {
+        this.orderService = orderService;
+        this.cartService = cartService;
+        this.userService = userService;
+        this.dtoResponseMapper = dtoResponseMapper;
+    }
+
     @PostMapping("/complete/{userId}")
     public OrderResponseDto complete(@PathVariable Long userId) {
-        return null;
+        return dtoResponseMapper.toDto(orderService.completeOrder(
+                cartService.getByUser(userService.get(userId))));
     }
 
     @GetMapping("/{userId}")
-    public OrderResponseDto get(@PathVariable Long userId) {
-        return null;
+    public List<OrderResponseDto> get(@PathVariable Long userId) {
+        List<Order> ordersHistory = orderService.getOrdersHistory(userService.get(userId));
+        return ordersHistory.stream()
+                .map(dtoResponseMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto responseDto = new OrderResponseDto();
+        responseDto.setId(order.getId());
+        responseDto.setOrderDate(order.getOrderDate());
+        responseDto.setUserId(order.getUser().getId());
+        responseDto.setTicketIds(getTicketIds(order));
+        return responseDto;
+    }
+
+    private List<Long> getTicketIds(Order order) {
+        return order.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList());
+    }
+}
