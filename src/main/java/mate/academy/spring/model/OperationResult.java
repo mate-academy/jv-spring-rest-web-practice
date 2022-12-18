4a40c6c34b7b8a117fee86109be10c5e2a469754
Pavlo.Polovyi <pasha32662@gmail.com>
2022-12-18 19:48:14 +0200

    Created OrderController, AuthenticationController, UserController, ShoppingCartController; created all necessary dtos and mappers.

---
+++ src/main/java/mate/academy/spring/model/OperationResult.java
@@ -0,0 +1,13 @@
+package mate.academy.spring.model;
+
+public class OperationResult {
+    private final Result result;
+
+    public OperationResult(Result result) {
+        this.result = result;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+}
