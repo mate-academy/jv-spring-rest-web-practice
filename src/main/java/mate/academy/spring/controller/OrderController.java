460893aa841d952e6a154213c298bda5cf4d502b
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-12-19 23:11:02 +0200

    Completed OrderController

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -6,10 +6,10 @@ import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
@@ -23,6 +23,15 @@ public class OrderController {
     @PostMapping("/complete")
     OrderResponseDto complete(@RequestParam Long id) {
         return dtoResponse.toDto(orderService.completeOrder(shoppingCartService.getByUser(userService.get(id))));
+    }
+
+    @GetMapping
+    List<OrderResponseDto> getOrderHistory(@RequestParam Long id) {
+        return orderService
+                .getOrdersHistory(userService.get(id))
+                .stream()
+                .map(o -> dtoResponse.toDto(o))
+                .collect(Collectors.toList());
     }
 
 }
