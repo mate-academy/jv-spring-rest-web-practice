192b77936be9e80cc90266f58f4007379bca6908
Veronika Tretiak <nikatretiak9@gmail.com>
2022-12-19 17:49:23 +0100

    Implemented task

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+
+    @Override
+    public OrderResponseDto toDto(Order object) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(object.getId());
+        orderResponseDto.setOrderDate(object.getOrderDate());
+        orderResponseDto.setTicketsIds(object.getTickets().stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList()));
+        orderResponseDto.setUserId(object.getUser().getId());
+        return orderResponseDto;
+    }
+}
