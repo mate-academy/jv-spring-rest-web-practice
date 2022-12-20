e78583a34427711763947b6e94ee9862ae3da29e
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-12-21 00:18:16 +0200

    fixed minor mistakes

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -24,7 +24,6 @@ public class UserController {
 
     @GetMapping("/by-email")
     public UserResponseDto getByEmail(@RequestParam String email) {
-        System.out.println(email);
         return userDtoResponseMapper.toDto(userService.findByEmail(email).get());
     }
 }
--- src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -4,10 +4,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserRequestDto {
-
     @NotNull
     private String email;
-
     @NotNull
     @Size(min = 4)
     private String password;
