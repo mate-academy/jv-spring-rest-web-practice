437562f0a211e6ab8f86d549e87624a7a153a4bd
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-12-21 21:02:35 +0200

    solved jv spring rest web practice task

---
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shopping-carts")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart>
+            shoppingCartDtoResponseMapper;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService,
+                                  DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart>
+                                          shoppingCartDtoResponseMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.shoppingCartDtoResponseMapper = shoppingCartDtoResponseMapper;
+    }
+
+    @PutMapping("/movie-sessions")
+
+    @GetMapping("by-user")
+    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
+        return shoppingCartDtoResponseMapper.toDto(shoppingCartService
+                .getByUser(userService.get(userId)));
+    }
+}
