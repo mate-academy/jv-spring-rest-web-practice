e313817b9a2cb78093cd0cc01a82ca5f3016f41c
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-12-21 17:44:29 +0200

    Hw done

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
+    public UserResponseDto toDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setEmail(user.getEmail());
+        return userResponseDto;
+    }
+}
