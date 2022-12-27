3291090209737378a9fafdc44221851da9561511
Danyl Kniaziev <kdanil0401@gmail.com>
2022-12-27 12:12:24 +0200

    fixed import block

--- src/main/java/mate/academy/spring/controller/ShoppingCartsController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartsController.java
@@ -7,7 +7,6 @@ import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
