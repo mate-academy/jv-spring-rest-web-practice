b130657996c3bbe7b095bc33bc7d6882c5d19577
Ivan Kharchenko <pro100detki@gmail.com>
2023-01-06 14:48:47 +0200

    replacing entities with their id in OrderResponse Dto and Mapper

--- src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -2,12 +2,12 @@ package mate.academy.spring.model.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import mate.academy.spring.model.Ticket;
 
 public class OrderResponseDto {
     private Long id;
-    private List<Ticket> tickets;
+    private List<Long> ticketIds;
     private LocalDateTime orderDate;
+    private Long userId;
 
     public Long getId() {
         return id;
@@ -17,12 +17,12 @@ public class OrderResponseDto {
         this.id = id;
     }
 
-    public List<Ticket> getTickets() {
-        return tickets;
+    public List<Long> getTicketIds() {
+        return ticketIds;
     }
 
-    public void setTickets(List<Ticket> tickets) {
-        this.tickets = tickets;
+    public void setTicketIds(List<Long> ticketIds) {
+        this.ticketIds = ticketIds;
     }
 
     public LocalDateTime getOrderDate() {
@@ -31,5 +31,13 @@ public class OrderResponseDto {
 
     public void setOrderDate(LocalDateTime orderDate) {
         this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
