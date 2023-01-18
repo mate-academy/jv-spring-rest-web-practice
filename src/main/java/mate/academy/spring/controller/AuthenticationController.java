f7165c6cfbe80d8737ad02812b360fc79b436aec
Katya Humeniuk <kapelka.boo@gmail.com>
2023-01-18 19:31:42 +0200

    Solution was added.

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseDtoMapper;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserResponseDtoMapper userResponseDtoMapper;
+
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    UserResponseDtoMapper userResponseDtoMapper) {
+        this.authenticationService = authenticationService;
+        this.userResponseDtoMapper = userResponseDtoMapper;
+    }
+
+    @PostMapping
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+        return userResponseDtoMapper.toDto(authenticationService
+                .register(userRequestDto.getEmail(), userRequestDto.getPassword()));
+    }
+}
