c71d1eaba3279a467989507a616616554810abb5
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-01-19 14:14:19 +0200

    added classes for responding data transfer objects, as order, shoppingCart, user

---
+++ src/main/java/mate/academy/spring/model/dto/response/UserResponseDto.java
@@ -0,0 +1,56 @@
+package mate.academy.spring.model.dto.response;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserResponseDto {
+    private Long id;
+    @Email
+    @NotNull
+    private String email;
+    @NotNull
+    @Size(min = 4)
+    private String password;
+    private byte[] salt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", email='" + email + '\''
+                + '}';
+    }
+}
