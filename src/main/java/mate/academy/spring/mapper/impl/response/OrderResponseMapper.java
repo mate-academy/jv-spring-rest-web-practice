6779e82373a71766ab2d194138b6f877dafe48c6
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-01-19 14:15:37 +0200

    added classes for mapping objects, as order, shoppingCart, user

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setTickets(order.getTickets());
+        orderResponseDto.setUser(order.getUser());
+        return orderResponseDto;
+    }
+}
