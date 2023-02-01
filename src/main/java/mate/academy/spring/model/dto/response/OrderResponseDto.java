a254ad90b51790f5c40b5e79fccdf737cca4b5ac
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-01 15:56:09 +0200

    Create OrderResponseDto

---
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class OrderResponseDto {
+
+    private Long id;
+    private List<Long> ticketIds;
+    @JsonFormat(pattern = "dd.MM.yyyy HH:mm:ss")
+    private LocalDateTime orderDate;
+
+    private long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Long> getTicketIds() {
+        return ticketIds;
+    }
+
+    public void setTicketIds(List<Long> ticketIds) {
+        this.ticketIds = ticketIds;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
