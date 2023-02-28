efcfd9ab4bad5365c91124daaaad1ceaebe7f6df
Ihor Vynogradskyi <avassorv@gmail.com>
2023-02-28 17:37:14 +0200

    web practice 1

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCartResponseMapper implements
+        DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart shoppingCart) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setId(shoppingCart.getId());
+        List<Long> ticketIdList = shoppingCart
+                .getTickets()
+                .stream()
+                .map(t -> t.getId())
+                .collect(Collectors.toList());
+        shoppingCartResponseDto.setTicketId(ticketIdList);
+        return shoppingCartResponseDto;
+    }
+}
