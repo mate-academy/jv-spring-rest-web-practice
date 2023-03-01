8779d47cc3e2dc9b85c66cf7c67b00819a0ebb02
solomiiavoloshyn <solomiiavoloshyn@gmail.com>
2023-03-01 21:05:56 +0200

    commit

---
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -0,0 +1,50 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import mate.academy.spring.service.MovieSessionService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shopping-carts")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final MovieSessionService movieSessionService;
+    private final DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart>
+            shoppingCartDtoResponseMapper;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService,
+                     UserService userService, MovieSessionService movieSessionService,
+                                  DtoResponseMapper<ShoppingCartResponseDto,
+                                          ShoppingCart> shoppingCartDtoResponseMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.movieSessionService = movieSessionService;
+        this.shoppingCartDtoResponseMapper = shoppingCartDtoResponseMapper;
+    }
+
+    @GetMapping("/by-user")
+    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
+        return shoppingCartDtoResponseMapper.toDto(
+                shoppingCartService.getByUser(userService.get(userId)));
+    }
+
+    @PutMapping("/movie-sessions")
+    public void add(@PathVariable Long userId,
+                       @PathVariable Long movieSessionId) {
+        MovieSession movieSession = movieSessionService.get(movieSessionId);
+        User user = userService.get(userId);
+        shoppingCartService.addSession(movieSession, user);
+    }
+}
