e3fd1131249238f4538a118e16476f1f223aa35b
Artem Voitiuk <artemon2504@gmail.com>
2023-03-01 22:05:04 +0200

    implements controllers for user, shopping cart, order and authentication controller

---
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -0,0 +1,48 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+    private final OrderResponseMapper responseMapper;
+    private final UserService userService;
+    private final ShoppingCartService shoppingCartService;
+
+    public OrderController(OrderService orderService,
+                           OrderResponseMapper responseMapper,
+                           UserService userService,
+                           ShoppingCartService shoppingCartService) {
+        this.orderService = orderService;
+        this.responseMapper = responseMapper;
+        this.userService = userService;
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @GetMapping
+    public List<OrderResponseDto> getOrderHistory(@RequestParam Long userId) {
+        return orderService.getOrdersHistory(userService.get(userId)).stream()
+                .map(responseMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/complete")
+    public OrderResponseDto completeOrder(@RequestBody Long userId) {
+        return responseMapper.toDto(orderService
+                .completeOrder(shoppingCartService
+                        .getByUser(userService.get(userId))));
+    }
+}
---
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.mapper.impl.response;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserResponseMapper implements DtoResponseMapper<UserResponseDto, User> {
+    @Override
+    public UserResponseDto toDto(User user) {
+        UserResponseDto responseDto = new UserResponseDto();
+        responseDto.setEmail(user.getEmail());
+        responseDto.setId(user.getId());
+        return responseDto;
+    }
+}
