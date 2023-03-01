112ed73d58ce4da51ec371160195cbb10d2f8b0f
Dmytro Krasnopir <logoltip@gmail.com>
2023-03-02 00:06:32 +0200

    mistake was fixed

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -4,11 +4,9 @@ import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class AuthenticationController {
     private final AuthenticationService service;
 
