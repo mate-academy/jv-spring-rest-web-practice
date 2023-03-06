96e2ac0307936119a205ef6de68d5d306ca32b0a
carbo <carbon7557@gmail.com>
2023-03-06 10:25:24 +0000

    Task is done.

---
+++ src/main/java/mate/academy/spring/model/dto/request/OrderRequestDto.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.model.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderRequestDto {
+    private List<Long> ticketIds;
+    private LocalDateTime orderDate;
+    private Long userId;
+
+    public List<Long> getTicketIds() {
+        return ticketIds;
+    }
+
+    public void setTicketIds(List<Long> ticketIds) {
+        this.ticketIds = ticketIds;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
