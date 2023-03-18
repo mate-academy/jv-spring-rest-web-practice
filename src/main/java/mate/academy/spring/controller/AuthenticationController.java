aeb825f4f19f93924cc7b90e0d342dfea8db2c57
Denis Pugovkin <den.pugovkin@gmail.com>
2023-03-18 20:42:45 +0200

    First commit. 1. All necessary controllers were created. 2. All DTO were created. 3. All mappers were created. 4. All methods were successfully tested.

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserResponseMapper responseMapper;
+
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    UserResponseMapper responseMapper) {
+        this.authenticationService = authenticationService;
+        this.responseMapper = responseMapper;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserRequestDto requestDto) {
+        authenticationService.register(requestDto.getEmail(), requestDto.getPassword());
+        return "Registration successful!";
+    }
+
+}
--- src/main/java/mate/academy/spring/dao/TicketDao.java
+++ src/main/java/mate/academy/spring/dao/TicketDao.java
@@ -3,4 +3,5 @@ package mate.academy.spring.dao;
 import mate.academy.spring.model.Ticket;
 
 public interface TicketDao extends GenericDao<Ticket> {
+
 }
