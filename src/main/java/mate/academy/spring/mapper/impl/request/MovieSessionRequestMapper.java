adb0141f4cc2eefad3795938fb498a823a4b9459
Dima Davydenko <dmytro.davydenko89@gmail.com>
2023-04-07 20:19:13 +0300

    ShoppingCartRequestDto and -Mapper were deleted. Services have been replaced by the Models constructors in OrderRequestMapper and MovieSessionRequestMapper.

--- src/main/java/mate/academy/spring/mapper/impl/request/MovieSessionRequestMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/request/MovieSessionRequestMapper.java
@@ -5,30 +5,17 @@ import mate.academy.spring.model.CinemaHall;
 import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.request.MovieSessionRequestDto;
-import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.MovieService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionRequestMapper implements DtoRequestMapper<MovieSessionRequestDto,
                                                                    MovieSession> {
-    private final CinemaHallService cinemaHallService;
-    private final MovieService movieService;
-
-    public MovieSessionRequestMapper(CinemaHallService cinemaHallService,
-                                     MovieService movieService) {
-        this.cinemaHallService = cinemaHallService;
-        this.movieService = movieService;
-    }
-
     @Override
     public MovieSession fromDto(MovieSessionRequestDto dto) {
         MovieSession movieSession = new MovieSession();
-        Movie movie = movieService.get(dto.getMovieId());
-        CinemaHall cinemaHall = cinemaHallService.get(dto.getCinemaHallId());
-        movieSession.setMovie(movie);
+        movieSession.setMovie(new Movie(dto.getMovieId()));
         movieSession.setShowTime(dto.getShowTime());
-        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setCinemaHall(new CinemaHall(dto.getCinemaHallId()));
         return movieSession;
     }
 }
--- src/main/java/mate/academy/spring/mapper/impl/request/OrderRequestMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/request/OrderRequestMapper.java
@@ -5,18 +5,12 @@ import java.util.stream.Collectors;
 import mate.academy.spring.mapper.DtoRequestMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.Ticket;
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.OrderRequestDto;
-import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderRequestMapper implements DtoRequestMapper<OrderRequestDto, Order> {
-    private final UserService userService;
-
-    public OrderRequestMapper(UserService userService) {
-        this.userService = userService;
-    }
-
     @Override
     public Order fromDto(OrderRequestDto dto) {
         Order order = new Order();
@@ -25,7 +19,7 @@ public class OrderRequestMapper implements DtoRequestMapper<OrderRequestDto, Ord
                 .collect(Collectors.toList());
         order.setTickets(tickets);
         order.setOrderDate(dto.getOrderDate());
-        order.setUser(userService.get(dto.getUserId()));
+        order.setUser(new User(dto.getUserId()));
         return order;
     }
 }
