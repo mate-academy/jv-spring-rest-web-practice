ae3797900163d18bf2d8e87eec700c453b889eb8
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-04-13 15:16:48 +0200

    rest-practice added solution. order doesn't extend user. slight changes in OrderController

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
+import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.User;
-import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
@@ -19,13 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
-    private final DtoResponseMapper<UserResponseDto, User> responseMapper;
+    private final DtoResponseMapper<OrderResponseDto, Order> responseMapper;
     private final ShoppingCartService shoppingCartService;
     private final UserService userService;
 
     public OrderController(OrderService orderService,
-                           DtoResponseMapper<UserResponseDto, User> responseMapper,
-                           ShoppingCartService shoppingCartService, UserService userService) {
+                           DtoResponseMapper<OrderResponseDto, Order> responseMapper,
+                           ShoppingCartService shoppingCartService,
+                           UserService userService) {
         this.orderService = orderService;
         this.responseMapper = responseMapper;
         this.shoppingCartService = shoppingCartService;
@@ -33,14 +35,15 @@ public class OrderController {
     }
 
     @PostMapping("/complete")
-    public UserResponseDto complete(@RequestParam Long userId) {
-        Order order = orderService.completeOrder(
-                shoppingCartService.getByUser(userService.get(userId)));
-        return responseMapper.toDto(order);
+    public OrderResponseDto complete(@RequestParam Long userId) {
+        ShoppingCart shoppingCartServiceByUser =
+                shoppingCartService.getByUser(userService.get(userId));
+        return responseMapper.toDto(orderService
+                .completeOrder(shoppingCartServiceByUser));
     }
 
     @GetMapping
-    public List<UserResponseDto> get(@RequestParam Long userId) {
+    public List<OrderResponseDto> get(@RequestParam Long userId) {
         User user = userService.get(userId);
         return orderService.getOrdersHistory(user)
                 .stream()
