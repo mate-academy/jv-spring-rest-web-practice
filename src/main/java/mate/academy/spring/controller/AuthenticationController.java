b5dc5e1973d50db4410c48708fa7853c1e8b853b
Vasyl Piznak <piznak3145@gmail.com>
2023-04-15 20:22:16 +0300

    Use correct mapping, add UserRequestDto and use it in register method as a parameter.

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,16 +1,14 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.mapper.impl.response.UserResponseMapper;
-import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
 public class AuthenticationController {
 
     private final UserResponseMapper userResponseMapper;
@@ -22,9 +20,10 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-    @PostMapping
-    public UserResponseDto register(@RequestBody User user) {
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
         return userResponseMapper
-                .toDto(authenticationService.register(user.getEmail(), user.getPassword()));
+                .toDto(authenticationService.register(userRequestDto.getEmail(),
+                        userRequestDto.getPassword()));
     }
 }
