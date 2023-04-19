fe68c61cd172ede7068860879da1c66adcb8d350
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-19 23:14:21 +0300

    fix UserController

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,10 +1,13 @@
 package mate.academy.spring.controller;
 
 import javax.validation.constraints.Email;
+import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final UserResponseMapper responseMapper;
+    private final DtoResponseMapper<UserResponseDto, User> responseMapper;
 
     @Autowired
-    public UserController(UserService userService, UserResponseMapper responseMapper) {
+    public UserController(UserService userService,
+                          @Qualifier("userResponseMapper") DtoResponseMapper<UserResponseDto,
+                                  User> responseMapper) {
         this.userService = userService;
         this.responseMapper = responseMapper;
     }
