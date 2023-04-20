0f357007b7a7d98c0f238c699ea39f136e8299a1
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-20 11:46:42 +0300

    checkstyle fix

--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.mapper.impl.response;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.ShoppingCart;
@@ -9,7 +8,8 @@ import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto,
+        ShoppingCart> {
     @Override
     public ShoppingCartResponseDto toDto(ShoppingCart cart) {
         ShoppingCartResponseDto responseDto = new ShoppingCartResponseDto();
