8d3230e3cf83520311db8c0bcf752d2c56b883af
Andrii Pylypenko <pilipenko1975@gmail.com>
2023-04-25 21:26:51 +0300

    My HW corrections were made in accordance with the mentor's comments

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -2,21 +2,17 @@ package mate.academy.spring.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import mate.academy.spring.mapper.DtoRequestMapper;
 import mate.academy.spring.mapper.DtoResponseMapper;
-import mate.academy.spring.mapper.impl.request.ShoppingCartRequestMapper;
-import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
 import mate.academy.spring.model.Order;
-import mate.academy.spring.model.ShoppingCart;
-import mate.academy.spring.model.dto.request.ShoppingCartRequestDto;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
 import mate.academy.spring.service.OrderService;
+import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,27 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private final OrderService orderService;
     private final DtoResponseMapper<OrderResponseDto, Order> orderResponseMapper;
-    private final DtoRequestMapper<ShoppingCartRequestDto, ShoppingCart> shoppingCartRequestMapper;
+    private final ShoppingCartService shoppingCartService;
     private final UserService userService;
 
     public OrderController(OrderService orderService,
-                           OrderResponseMapper orderResponseMapper,
-                           ShoppingCartRequestMapper shoppingCartRequestMapper,
+                           DtoResponseMapper<OrderResponseDto, Order> orderResponseMapper,
+                           ShoppingCartService shoppingCartService,
                            UserService userService) {
         this.orderService = orderService;
         this.orderResponseMapper = orderResponseMapper;
-        this.shoppingCartRequestMapper = shoppingCartRequestMapper;
+        this.shoppingCartService = shoppingCartService;
         this.userService = userService;
     }
 
-    @PostMapping("/{id}")
-    OrderResponseDto completeOrder(@PathVariable Long id,
-                                   @RequestBody ShoppingCartRequestDto shoppingCartRequestDto) {
-        ShoppingCart shoppingCart = shoppingCartRequestMapper.fromDto(shoppingCartRequestDto);
-        shoppingCart.setId(id);
-        shoppingCart.setUser(userService.get(id));
-        Order order = orderService.completeOrder(shoppingCart);
-        return orderResponseMapper.toDto(order);
+    @PostMapping("/complete")
+    public OrderResponseDto complete(@RequestParam Long userId) {
+        return orderResponseMapper.toDto(
+                orderService.completeOrder(shoppingCartService.getByUser(
+                        userService.get(userId))));
     }
 
     @GetMapping("/{id}")
