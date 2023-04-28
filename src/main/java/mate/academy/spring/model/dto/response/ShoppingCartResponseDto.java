cfd6bc54fc24a60e500a1fbc51ce5509d4c7af52
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-04-28 16:40:43 +0300

    refactored ShoppingCartResponseDto

--- src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class ShoppingCartResponseDto {
     private Long id;
     private List<Long> ticketIds;
-    private Long userId;
 
     public Long getId() {
         return id;
@@ -21,13 +20,5 @@ public class ShoppingCartResponseDto {
 
     public void setTicketIds(List<Long> ticketIds) {
         this.ticketIds = ticketIds;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
     }
 }
