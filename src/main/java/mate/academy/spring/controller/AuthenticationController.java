ea740cc5838f551f054c9f55ef9ac6ec847b412c
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-04-30 16:38:04 +0300

    Created AuthenticationController

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping
+    public User register(@RequestParam UserRequestDto userRequestDto) {
+        return authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
+    }
+}
