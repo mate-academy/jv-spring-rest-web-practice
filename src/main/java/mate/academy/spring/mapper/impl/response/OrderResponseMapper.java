60c4040e94fe1b3b41b8ba4c71184378e11d995c
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-04-30 16:23:58 +0300

    Create OrderController

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -3,7 +3,9 @@ package mate.academy.spring.mapper.impl.response;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
     @Override
     public OrderResponseDto toDto(Order order) {
--- src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/UserResponseMapper.java
@@ -3,7 +3,9 @@ package mate.academy.spring.mapper.impl.response;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserResponseMapper implements DtoResponseMapper<UserResponseDto,User> {
     @Override
     public UserResponseDto toDto(User user) {
