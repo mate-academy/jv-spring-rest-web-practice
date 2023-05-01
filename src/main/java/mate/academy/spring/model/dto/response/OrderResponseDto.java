8610c5ebd79d2525a89d3cb8c7eef6d686d96c9c
Sushko Roman <formatesus@gmail.com>
2023-05-02 01:23:58 +0300

    created DTO`s

---
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderResponseDto {
+    private Long id;
+    private List<Long> ticketsId;
+    private LocalDateTime orderDate;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Long> getTickets() {
+        return ticketsId;
+    }
+
+    public void setTickets(List<Long> tickets) {
+        this.ticketsId = tickets;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
