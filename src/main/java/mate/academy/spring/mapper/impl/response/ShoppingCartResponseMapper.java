53bb62ef415dc6a5e89389ba7c6d1feeb5f9fa4c
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-01 19:48:34 +0300

    implement dto's, mappers, controllers

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
+import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCartResponseMapper implements DtoResponseMapper<ShoppingCartResponseDto,
+        ShoppingCart> {
+    @Override
+    public ShoppingCartResponseDto toDto(ShoppingCart object) {
+        ShoppingCartResponseDto shoppingCartResponseDto = new ShoppingCartResponseDto();
+        shoppingCartResponseDto.setId(object.getId());
+        shoppingCartResponseDto.setTicketsId(object.getTickets()
+                .stream()
+                .map(i -> i.getId())
+                .collect(Collectors.toList()));
+        shoppingCartResponseDto.setUserId(object.getUser().getId());
+        return shoppingCartResponseDto;
+    }
+}
