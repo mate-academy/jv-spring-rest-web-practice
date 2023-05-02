4fdcfdeea5779e4fd950d7df4b98bd860e1c0efd
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-05-02 13:19:03 +0300

    add user Dto, Mapper and Controller

---
+++ src/main/java/mate/academy/spring/model/dto/request/UserRequestDto.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class UserRequestDto {
+    @Email
+    @NotNull
+    private String email;
+    @NotNull
+    @Min(value = 8)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
