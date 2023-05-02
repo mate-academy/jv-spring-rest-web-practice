a5e585552b0243654173e545030ca9705553f92f
potrap <potrapmax@gmail.com>
2023-05-02 14:36:54 +0300

    removed user in dto

--- src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/response/ShoppingCartResponseDto.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class ShoppingCartResponseDto {
     private Long id;
     private List<Long> ticketIds;
-    private UserResponseDto user;
 
     public Long getId() {
         return id;
@@ -21,13 +20,5 @@ public class ShoppingCartResponseDto {
 
     public void setTicketIds(List<Long> ticketIds) {
         this.ticketIds = ticketIds;
-    }
-
-    public UserResponseDto getUser() {
-        return user;
-    }
-
-    public void setUser(UserResponseDto user) {
-        this.user = user;
     }
 }
