dbd9e097dd52eb4c11ca1477864765a0e89dc633
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-05-02 17:06:36 +0300

    refactoring

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,6 +1,10 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.exception.RegistrationException;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final DtoResponseMapper<UserResponseDto, User> userDtoResponseMapper;
 
-    public AuthenticationController(AuthenticationService authenticationService) {
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    DtoResponseMapper<UserResponseDto, User> userResponseMapper) {
         this.authenticationService = authenticationService;
+        this.userDtoResponseMapper = userResponseMapper;
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody UserRequestDto userRequestDto) {
-        authenticationService.register(
-                userRequestDto.getEmail(),
-                userRequestDto.getPassword());
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+        try {
+            User registeredUser = authenticationService.register(
+                    userRequestDto.getEmail(), userRequestDto.getPassword());
+            return userDtoResponseMapper.toDto(registeredUser);
+        } catch (RegistrationException e) {
+            throw new RuntimeException("Can't register user");
+        }
     }
 }
