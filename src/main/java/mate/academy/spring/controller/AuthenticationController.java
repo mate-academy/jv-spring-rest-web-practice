541c90982213ea1d9323d03084742459622a0524
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-05-02 14:50:58 +0300

    minor changes

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -8,11 +8,9 @@ import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final DtoResponseMapper<UserResponseDto, User> userResponseMapper;
@@ -25,7 +23,7 @@ public class AuthenticationController {
         this.userResponseMapper = userResponseMapper;
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
         return userResponseMapper.toDto(authenticationService.register(
                 userRequestDto.getEmail(), userRequestDto.getPassword()));
