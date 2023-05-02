8550f273793ae62b0e212ad0925b1b7e7bf4a5d8
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-02 19:23:15 +0300

    added fixes after mentor review;

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -2,6 +2,8 @@ package mate.academy.spring.controller;
 
 import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.security.AuthenticationService;
+import mate.academy.spring.service.UserService;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class AuthenticationController {
     private final AuthenticationService authService;
+    private final UserService userService;
 
-    public AuthenticationController(AuthenticationService authService) {
+    public AuthenticationController(AuthenticationService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
     public void register(@RequestBody UserRequestDto userRequestDto) {
+        if (userService.findByEmail(userRequestDto.getEmail()).isPresent()) {
+            throw new RuntimeException("User already exist!");
+        }
         authService.register(userRequestDto.getEmail(), userRequestDto.getPassword());
     }
 }
--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package mate.academy.spring.controller;
 
-import mate.academy.spring.mapper.impl.response.ShoppingCartResponseMapper;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
@@ -14,15 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/shopping-carts")
 public class ShoppingCartController {
-    private final ShoppingCartResponseMapper shoppingCartResponseMapper;
+    private final DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> shoppingCartResponseMapper;
     private final ShoppingCartService shoppingCartService;
     private final MovieSessionService movieSessionService;
     private final UserService userService;
 
-    public ShoppingCartController(ShoppingCartResponseMapper shoppingCartResponseMapper,
-                                  ShoppingCartService shoppingCartService,
-                                  MovieSessionService movieSessionService,
-                                  UserService userService) {
+    public ShoppingCartController(
+            DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> shoppingCartResponseMapper,
+            ShoppingCartService shoppingCartService,
+            MovieSessionService movieSessionService,
+            UserService userService) {
         this.shoppingCartResponseMapper = shoppingCartResponseMapper;
         this.shoppingCartService = shoppingCartService;
         this.movieSessionService = movieSessionService;
