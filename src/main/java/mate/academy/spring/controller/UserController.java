6b62e0ee88ed02e884a29b59829c81f356b227c4
Denys <denchik9026@gmail.com>
2023-05-02 11:52:43 +0300

    checkstyle fix

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -2,7 +2,6 @@ package mate.academy.spring.controller;
 
 import java.util.NoSuchElementException;
 import mate.academy.spring.mapper.DtoResponseMapper;
-import mate.academy.spring.mapper.impl.response.UserResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.UserService;
