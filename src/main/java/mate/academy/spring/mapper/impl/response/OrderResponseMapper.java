ea3f624231b323ef15a23f84a68df84800a88744
Artem Grunin <artem.grunin.2006@gmail.com>
2023-05-02 09:11:32 +0300

    fixeв фттщÐd mappers annotations)

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -5,9 +5,10 @@ import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
     @Override
     public OrderResponseDto toDto(Order order) {
--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -5,9 +5,10 @@ import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class ShoppingCartResponseMapper implements
         DtoResponseMapper<ShoppingCartResponseDto, ShoppingCart> {
     @Override
