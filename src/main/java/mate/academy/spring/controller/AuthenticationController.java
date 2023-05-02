3947fbd598ac7343d3fe1f929511065df5be8a7c
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-02 18:42:00 +0300

    fixed

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,10 +1,8 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.exception.DataProcessingException;
-import mate.academy.spring.mapper.impl.response.UserResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
-import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,25 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
-    private final UserResponseMapper userResponseMapper;
     private final UserService userService;
 
     public AuthenticationController(AuthenticationService authenticationService,
-                                    UserResponseMapper userResponseMapper,
                                     UserService userService) {
         this.authenticationService = authenticationService;
-        this.userResponseMapper = userResponseMapper;
         this.userService = userService;
     }
 
     @PostMapping("/register")
-    public UserResponseDto save(@RequestBody UserRequestDto userRequestDto) {
+    public void register(@RequestBody UserRequestDto userRequestDto) {
         if (userService.findByEmail(userRequestDto.getEmail()).isPresent()) {
             throw new DataProcessingException("User with this email already exist: "
                     + userRequestDto.getEmail());
         }
         User user = authenticationService.register(userRequestDto.getEmail(),
                 userRequestDto.getPassword());
-        return userResponseMapper.toDto(user);
     }
 }
