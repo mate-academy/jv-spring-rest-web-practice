3fcf1718353fce42bb26dd56e6a7d0dc70285d6d
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-05-04 17:10:50 +0300

    changed method "toList" on "collect(Collectors.toList())"

--- src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.mapper.impl.response;
 
+import java.util.stream.Collectors;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.Ticket;
@@ -15,7 +16,8 @@ public class OrderResponseMapper
         responseDto.setId(order.getId());
         responseDto.setOrderDate(order.getOrderDate());
         responseDto.setTicketIds(order.getTickets()
-                .stream().map(Ticket::getId).toList());
+                .stream().map(Ticket::getId)
+                .collect(Collectors.toList()));
         return responseDto;
     }
 }
