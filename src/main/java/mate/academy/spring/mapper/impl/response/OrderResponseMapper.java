6a31ed224d0930b3d64a70089a7ba7d3b110edef
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-05-12 02:38:05 +0300

    Authentication and Order controllers were implemented Dtos and Mappers for User and Order were created

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,21 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setOrderDate(order.getOrderDate());
+        orderResponseDto.setUserId(order.getUser().getId());
+        orderResponseDto.setTicketsIds(
+                order.getTickets().stream()
+                .map(orderTicket -> orderTicket.getId())
+                .collect(Collectors.toList()));
+        return orderResponseDto;
+    }
+}
