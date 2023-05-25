e1cbf023a12a474b028ab3e488a60a1dd6fb3ee5
Nikita Melnikov <bam19921303@gmail.com>
2023-05-26 00:12:24 +0300

    All tasks are done

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,38 @@
+package mate.academy.spring.controller;
+
+import java.util.Optional;
+import mate.academy.spring.exception.AuthenticationException;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final DtoResponseMapper<UserResponseDto, User> responseMapper;
+
+    public UserController(UserService userService,
+                          DtoResponseMapper<UserResponseDto, User> responseMapper) {
+        this.userService = userService;
+        this.responseMapper = responseMapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isPresent()) {
+            return responseMapper.toDto(userOptional.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't find user by mail: "
+                    + email);
+        }
+    }
+}
