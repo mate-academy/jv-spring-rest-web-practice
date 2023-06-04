7905e8160339d848c90d85378f4c0172b16ca920
vova5179 <vova5179@gmail.com>
2023-06-04 12:54:12 +0300

    removed secure data from UserResponceDto

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,13 +1,16 @@
 package mate.academy.spring.controller;
 
+import java.util.Optional;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +26,12 @@ public class UserController {
 
     @GetMapping("/by-email")
     public UserResponseDto userByEmail(@RequestParam String email) {
-        return userResponseMapper.toDto(userService.findByEmail(email).get());
+        Optional<User> userByEmail = userService.findByEmail(email);
+        if (userByEmail.isPresent()) {
+            return userResponseMapper.toDto(userByEmail.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't find user by email "
+                    + email);
+        }
     }
 }
--- src/main/java/mate/academy/spring/mapper/impl/request/UserRequestMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/request/UserRequestMapper.java
@@ -2,13 +2,13 @@ package mate.academy.spring.mapper.impl.request;
 
 import mate.academy.spring.mapper.DtoRequestMapper;
 import mate.academy.spring.model.User;
-import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.model.dto.request.UserRequestDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserRequestMapper implements DtoRequestMapper<UserResponseDto, User> {
+public class UserRequestMapper implements DtoRequestMapper<UserRequestDto, User> {
     @Override
-    public User fromDto(UserResponseDto dto) {
+    public User fromDto(UserRequestDto dto) {
         User user = new User();
         user.setEmail(dto.getEmail());
         user.setPassword(dto.getPassword());
