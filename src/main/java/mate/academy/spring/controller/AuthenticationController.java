73aed6525fbc9a91a38fdc74adce8c361d7799e4
Oles <a.vykhovanok@gmail.com>
2023-06-08 18:45:35 +0300

    fixed

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,7 +1,8 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.exception.AuthenticationException;
-import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
@@ -9,30 +10,29 @@ import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserService userService;
-    private final UserResponseMapper userResponseMapper;
+    private final DtoResponseMapper<UserResponseDto, User> authDtoResponseMapper;
 
     @Autowired
     public AuthenticationController(AuthenticationService authenticationService,
                                     UserService userService,
-                                    UserResponseMapper userResponseMapper) {
+                                    DtoResponseMapper<UserResponseDto, User>
+                                            authDtoResponseMapper) {
         this.authenticationService = authenticationService;
         this.userService = userService;
-        this.userResponseMapper = userResponseMapper;
+        this.authDtoResponseMapper = authDtoResponseMapper;
     }
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody UserRequestDto userRequestDto)
             throws AuthenticationException {
         if (userService.findByEmail(userRequestDto.getEmail()).isEmpty()) {
-            return userResponseMapper
+            return authDtoResponseMapper
                 .toDto(authenticationService
                     .register(userRequestDto.getEmail(), userRequestDto.getPassword()));
         }
