d124903f81de451c2e417a2f7d64958ed9b25349
richn <rolik.alexx@gmail.com>
2023-06-14 01:08:35 +0300

    Initial commit

---
+++ src/main/java/mate/academy/spring/mapper/impl/response/OrderResponseMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.spring.mapper.impl.response;
+
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.Order;
+import mate.academy.spring.model.dto.response.OrderResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements DtoResponseMapper<OrderResponseDto, Order> {
+    @Override
+    public OrderResponseDto toDto(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setTimeOrder(order.getOrderDate());
+        orderResponseDto.setTicketsId(order.getTickets()
+                .stream()
+                .map(ticket -> ticket.getId())
+                .collect(Collectors.toList()));
+        return orderResponseDto;
+    }
+}
