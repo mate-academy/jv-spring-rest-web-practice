17e90749802f6efcf40cb082f0a3a8831b9ab4e8
Melnychuk <melnychukmyhailo@gmail.com>
2023-06-25 17:38:11 +0300

    UserController was created.

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserResponseMapper<UserResponseDto, User> userResponseDtoMapper;
+    private final UserService userService;
+
+    public UserController(UserResponseMapper<UserResponseDto, User> userResponseDtoMapper,
+                          UserService userService) {
+        this.userResponseDtoMapper = userResponseDtoMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        return userResponseDtoMapper.toDto(userService.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Can't find user by email : " + email)));
+    }
+}
