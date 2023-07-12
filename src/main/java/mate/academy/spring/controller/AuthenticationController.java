957ddc0d53bc026f9a0a20eafecded9c9ec8012a
Roman Bondarenko <rbondarenko3.work@gmail.com>
2023-07-12 13:34:07 +0300

    'REST Web practice' task completed

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.RegistrationRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final DtoResponseMapper<UserResponseDto, User> userResponseMapper;
+
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    DtoResponseMapper<UserResponseDto, User> userResponseMapper) {
+        this.authenticationService = authenticationService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody RegistrationRequestDto registrationRequestDto) {
+        return userResponseMapper.toDto(authenticationService.register(
+                registrationRequestDto.getEmail(), registrationRequestDto.getPassword()
+        ));
+    }
+}
