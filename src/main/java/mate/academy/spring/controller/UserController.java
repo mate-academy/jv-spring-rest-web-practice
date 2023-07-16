59da5b49b8059be22ba4894df6fcda839ef47b4b
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2023-07-16 13:22:25 +0300

    fixed all mistakes

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.controller;
 
-import mate.academy.spring.mapper.impl.response.UserResponseMapper;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
+++ src/main/java/mate/academy/spring/mapper/impl/response/ShoppingCartResponseMapper.java
@@ -1,13 +1,12 @@
 package mate.academy.spring.mapper.impl.response;
 
+import java.util.ArrayList;
+import java.util.List;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.Ticket;
 import mate.academy.spring.model.dto.response.ShoppingCartResponseDto;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class ShoppingCartResponseMapper
