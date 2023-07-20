52c52bd895317251b1abd6fbb75afa350a12e1b8
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-07-20 17:50:11 +0300

    Jv-spring-rest-web-practice - fixed mistakes after student review.

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -2,7 +2,7 @@ package mate.academy.spring.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import mate.academy.spring.mapper.impl.response.OrderResponseMapper;
+import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.ShoppingCart;
 import mate.academy.spring.model.dto.response.OrderResponseDto;
@@ -19,11 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
-    private final OrderResponseMapper responseMapper;
+    private final DtoResponseMapper<OrderResponseDto, Order> responseMapper;
     private final ShoppingCartService shoppingCartService;
     private final UserService userService;
 
-    public OrderController(OrderService orderService, OrderResponseMapper responseMapper,
+    public OrderController(OrderService orderService,
+                           DtoResponseMapper<OrderResponseDto, Order> responseMapper,
                            ShoppingCartService shoppingCartService, UserService userService) {
         this.orderService = orderService;
         this.responseMapper = responseMapper;
