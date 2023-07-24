2f0edab0d0d6517bbf6db49171350c6f23236984
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-07-24 16:14:24 +0300

    UserController working

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,13 +1,29 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.mapper.impl.response.UserResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
-//    @GetMapping("by-email")
-//    public UserResponseDto getByEmail( @RequestBody String email) {
-//
-//    }
+    private final UserService userService;
+    private final UserResponseMapper userResponseMapper;
+
+    public UserController(UserService userService, UserResponseMapper userResponseMapper) {
+        this.userService = userService;
+        this.userResponseMapper = userResponseMapper;
+    }
+
+    @GetMapping("by-email")
+    public UserResponseDto getByEmail(@RequestParam String email) {
+        User userFromDB = userService.findByEmail(email)
+                .orElseThrow();
+        return userResponseMapper.toDto(userFromDB);
+    }
 }
