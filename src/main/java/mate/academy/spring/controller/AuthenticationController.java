4068843ebe06f824883f32b2ce306680cddbd885
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-07-25 16:59:29 +0300

    mistakes corrected after review

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -6,20 +6,18 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRegistrationDto;
 import mate.academy.spring.model.dto.response.UserResponseDto;
 import mate.academy.spring.security.AuthenticationService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    @Qualifier("UserResponseMapper")
     private final DtoResponseMapper<UserResponseDto, User> dtoResponseMapper;
     private final AuthenticationService authenticationService;
 
-    public AuthenticationController(@Qualifier("userResponseMapper")
-                                    DtoResponseMapper<UserResponseDto, User> dtoResponseMapper,
-                                    AuthenticationService authenticationService) {
+    public AuthenticationController(
+            DtoResponseMapper<UserResponseDto, User> dtoResponseMapper,
+            AuthenticationService authenticationService) {
         this.dtoResponseMapper = dtoResponseMapper;
         this.authenticationService = authenticationService;
     }
--- src/main/java/mate/academy/spring/model/dto/request/UserRegistrationDto.java
+++ src/main/java/mate/academy/spring/model/dto/request/UserRegistrationDto.java
@@ -2,15 +2,12 @@ package mate.academy.spring.model.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import mate.academy.spring.validation.ValidPassword;
 
-@ValidPassword
 public class UserRegistrationDto {
     @Email
     private String email;
     @NotNull
     private String password;
-    @NotNull
     private String repeatPassword;
 
     public String getEmail() {
@@ -23,17 +20,5 @@ public class UserRegistrationDto {
 
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
     }
 }
