085f581ba7338ef6716e211be0c8fcded2c4e6e1
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-07-26 07:51:30 +0200

    fixed after mentor review x2

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.controller;
 
+import jakarta.validation.Valid;
 import mate.academy.spring.mapper.DtoResponseMapper;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.request.UserRequestDto;
@@ -21,7 +22,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+    public UserResponseDto register(@RequestBody @Valid UserRequestDto userRequestDto) {
         return userResponseMapper.toDto(
                 authenticationService.register(
                         userRequestDto.getEmail(), userRequestDto.getPassword()));
