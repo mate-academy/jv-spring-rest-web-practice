19bb9f11f372c408fc403540578f43ed9471e454
Serhii Dudko <nechupara@gmail.com>
2023-07-26 21:27:48 +0300

    implemented first working version of hw

---
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.controller;
+
+import javax.validation.Valid;
+import mate.academy.spring.mapper.DtoResponseMapper;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.request.UserRequestDto;
+import mate.academy.spring.model.dto.response.UserResponseDto;
+import mate.academy.spring.security.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final DtoResponseMapper<UserResponseDto, User> dtoResponseMapper;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    DtoResponseMapper<UserResponseDto, User> dtoResponseMapper) {
+        this.authenticationService = authenticationService;
+        this.dtoResponseMapper = dtoResponseMapper;
+    }
+
+    @PostMapping
+    public UserResponseDto registerUser(@RequestBody @Valid UserRequestDto userRequestDto) {
+        User user = authenticationService.register(userRequestDto.getEmail(),
+                userRequestDto.getPassword());
+        return dtoResponseMapper.toDto(user);
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/dto/response/OrderResponseDto.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderResponseDto {
+    private Long id;
+    private LocalDateTime orderDate;
+    private Long userId;
+    private List<Long> ticketsIds;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Long> getTicketsIds() {
+        return ticketsIds;
+    }
+
+    public void setTicketsIds(List<Long> ticketsIds) {
+        this.ticketsIds = ticketsIds;
+    }
+}
